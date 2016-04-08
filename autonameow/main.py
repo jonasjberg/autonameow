bc20708cbd1fb043f76512d845be2ae9ed47181f
Jonas Sjöberg <jomeganas@gmail.com>
2016-04-09 00:55:49 +0200

    Prepare for merge ..

--- autonameow/main.py
+++ autonameow/main.py
@@ -12,27 +12,26 @@ def main():
     # Loop over arguments ..
     for arg in sys.argv[1:]:
         if util.disk.is_readable_file(arg):
-            print '------------------------------------------------------------'
-            # Determine mime type and run analysis based on result.
-            type = util.disk.determine_file_type(arg)
-            # print "determined file type: ", type
             # print '------------------------------------------------------------'
             # io.disk.print_ltstat_info(arg)
-
-            # print '------------------------------------------------------------'
-            analyze_file(arg, type)
-
+            analyze_file(arg)
         else:
             # Basic sanity check failed, skip to next argument
             continue
 
 
-def analyze_file(path, type):
+def analyze_file(path):
     # filenamedate = analyze.common.extract_date_from_string(os.path.basename(path))
     # print "Date in filename: ", filenamedate
 
     file_name_noext = util.disk.get_file_name_noext(path)
     analyze.fuzzy_date_parser.try_parse_date(file_name_noext)
+
+
+    # Determine mime type and run analysis based on result.
+    type = util.disk.determine_file_type(path)
+    # print "determined file type: ", type
+    # print '------------------------------------------------------------'
 
     if type == "JPEG":
         print "will run image routine"
