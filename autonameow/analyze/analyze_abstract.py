3d65b804b3e85bbf3826eff55012bda2de766d8c
Jonas Sjöberg <jomeganas@gmail.com>
2016-05-31 14:36:32 +0200

    Add new class "AbstractAnalyzer".
    Adds a new class "AbstractAnalyzer", which is to be used as an interface
    to all file (mime) type specific analyzers.

---
+++ autonameow/analyze/analyze_abstract.py
@@ -0,0 +1,18 @@
+class AbstractAnalyzer(object):
+    def __init__(self, file_object, filters):
+        self.file_object = file_object
+        self.filters = filters
+
+    def get_datetime(self):
+        raise NotImplementedError
+
+    def get_title(self):
+        raise NotImplementedError
+
+    def get_author(self):
+        raise NotImplementedError
+
+    # def run(self):
+    #     """
+    #     Run the analysis.
+    #     """
