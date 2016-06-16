c7eb2eb901b35a9685e385a11b5e9f47ce4550b8
Jonas Sjöberg <jomeganas@gmail.com>
2016-06-16 18:37:00 +0200

    Add 'config_defaults.py'
    Adds 'config_defaults.py', a configuration file simply composed of a
    giant dictionary.
    
    Should contain matching rules as well as templates the new names.  A
    file that matches a rule named "my_rule" should be renamed according to
    the corresponding renaming template "my_rule".
    
    Currently a work in progress.

---
+++ autonameow/config_defaults.py
@@ -0,0 +1,6 @@
+match_rules = {'ogv_record_my_desktop': {'mime': 'ogv',
+                                         'name': None,
+                                         'path': None,
+                                         'prefer_datetime': 'Fs_Accessed'}}
+
+match_rename_template = {'ogv_record_my_desktop': }
