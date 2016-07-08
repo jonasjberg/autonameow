7135d4f3ac3ca71d0ef2a8916685d4327868a13f
Jonas Sjöberg <jomeganas@gmail.com>
2016-07-08 17:47:07 +0200

    Add comment header to 'version.py'.
    Modifies 'version.py':
    * Adds comment header
    * Fixes formatting

--- autonameow/version.py
+++ autonameow/version.py
@@ -1,9 +1,14 @@
+# -*- coding: utf-8 -*-
+# This file is part of autonameow.
+# Copyright 2016, Jonas Sjoberg.
+
 # Store the version here so:
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
 #
 # Source: http://stackoverflow.com/a/16084844
+
 __title__ = 'autonameow'
 __version__ = '0.0.1'
 __author__ = 'Jonas Sjoberg'
