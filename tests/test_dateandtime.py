a04de6c64b277d8c45b470ca2600a8cc97239e39
Jonas Sjöberg <jomeganas@gmail.com>
2016-07-22 21:40:35 +0200

    Restructure tests in 'test_dateandtime.py'.

--- tests/test_dateandtime.py
+++ tests/test_dateandtime.py
@@ -14,44 +14,55 @@ class TestDateAndTime(TestCase):
         self.assertEqual(hyphenate_date('20161224T121314'), '20161224T121314')
         self.assertEqual(hyphenate_date('return as-is'), 'return as-is')
 
+
+class TestMatchUnixTimestamp(TestCase):
     def test_match_unix_timestamp(self):
+        # Setup and sanity check:
         expected = datetime.strptime('20160528 201245', '%Y%m%d %H%M%S')
         expected2 = datetime.strptime('20160427 214010', '%Y%m%d %H%M%S')
         self.assertIsInstance(expected, datetime)
         self.assertIsInstance(expected2, datetime)
         self.assertNotEqual(expected, expected2)
 
-        self.assertIsNone(match_any_unix_timestamp(None))
-        self.assertIsNone(match_any_unix_timestamp(''))
-        self.assertIsNone(match_any_unix_timestamp(' '))
-        self.assertIsNone(match_any_unix_timestamp('abc'))
-        self.assertIsNone(match_any_unix_timestamp('123456'))
-
+        # Tests:
         self.assertEqual(expected, match_any_unix_timestamp('1464459165'))
         self.assertEqual(expected, match_any_unix_timestamp('1464459165038'))
         self.assertEqual(expected,
                          match_any_unix_timestamp('IMG_1464459165038.jpg'))
         self.assertEqual(expected,
                          match_any_unix_timestamp('IMG_1464459165038.jpg'
-                                              '1464459165038'))
+                                                  '1464459165038'))
         self.assertEqual(expected,
-                         match_any_unix_timestamp('1464459165038 1464459165038'))
+                         match_any_unix_timestamp('1464459165038 '
+                                                  '1464459165038'))
 
         self.assertEqual(expected2,
                          match_any_unix_timestamp('date --date="@1461786010" '
-                                              '--rfc-3339=seconds'))
+                                                  '--rfc-3339=seconds'))
         self.assertNotEqual(expected2, match_any_unix_timestamp('1464459165'))
 
+    def test_match_unix_timestamp_with_invalid_argument(self):
+        self.assertIsNone(match_any_unix_timestamp(None))
+        self.assertIsNone(match_any_unix_timestamp(''))
+        self.assertIsNone(match_any_unix_timestamp(' '))
+        self.assertIsNone(match_any_unix_timestamp('abc'))
+        self.assertIsNone(match_any_unix_timestamp('123456'))
+
+
+class TestMatchSpecialCase(TestCase):
     def test_match_special_case(self):
+        # Setup and sanity check:
         expected = datetime.strptime('20160722 131730', '%Y%m%d %H%M%S')
         self.assertIsInstance(expected, datetime)
 
+        # Tests:
+        self.assertIsNotNone(match_special_case('2016-07-22_131730'))
+        self.assertEqual(expected, match_special_case('2016-07-22_131730'))
+        self.assertEqual(expected, match_special_case('2016-07-22T131730'))
+        self.assertNotEqual(expected, match_special_case('2016-07-22T131731'))
+
+    def test_match_special_case_with_invalid_argument(self):
         self.assertIsNone(match_special_case(None))
         self.assertIsNone(match_special_case(''))
         self.assertIsNone(match_special_case(' '))
         self.assertIsNone(match_special_case('abc'))
-        self.assertIsNotNone(match_special_case('2016-07-22_131730'))
-
-        self.assertEqual(expected, match_special_case('2016-07-22_131730'))
-        self.assertEqual(expected, match_special_case('2016-07-22T131730'))
-        self.assertNotEqual(expected, match_special_case('2016-07-22T131731'))
