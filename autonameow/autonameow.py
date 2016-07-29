d6dd5021edacb7181048cc525202ff91bbd874bb
Jonas Sjöberg <jomeganas@gmail.com>
2016-07-29 15:44:28 +0200

    Remove commented/dead code.
    Remove commented/dead call to removed method 'print_oldest_datetime' in
    the Analysis class.

--- autonameow/autonameow.py
+++ autonameow/autonameow.py
@@ -198,7 +198,6 @@ class Autonameow(object):
                 if self.args.list_datetime:
                     print('File: "{}"'.format(current_file.path))
                     analysis.print_all_datetime_info()
-                    # analysis.print_oldest_datetime()
                     # analysis.prefix_date_to_filename()
                     print('')
 
