273e0b93d3be3e5f6ee3800717eda434ee4c8892
Jonas Sjöberg <jomeganas@gmail.com>
2016-08-08 11:42:47 +0200

    Move command-line handling to new file 'options.py'.
    Moves all argparse command-line parsing to a new file 'options.py'.

---
+++ autonameow/core/options.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+# This file is part of autonameow.
+# Copyright 2016, Jonas Sjoberg.
+
+import argparse
+import logging
+from colorama import Fore
+
+from datetime import datetime
+
+from core.util import dateandtime
+
+
+def arg_is_year(value):
+    """
+    Check if "value" is a year, as in 4 digits and 0 >= year > 9999 ..
+    :return:
+    """
+    ivalue = None
+    try:
+        ivalue = int(value.strip())
+    except ValueError:
+        pass
+    else:
+        if ivalue:
+            if len(str(ivalue)) == 4 and ivalue >= 0:
+                return ivalue
+    raise argparse.ArgumentTypeError('"{}" is not a valid year'.format(value))
+    return None
+
+
+def init_argparser():
+    """
+    Initialize the argparser. Add all arguments/options.
+    :return: the argument parser
+    """
+    parser = argparse.ArgumentParser(
+        prog='autonameow',
+        description='Automatic renaming of files from analysis of '
+                    'several sources of information.',
+        epilog='Example usage: TODO ..')
+
+    # Add option group for controlling what is printed to stdout.
+    optgrp_output = parser.add_mutually_exclusive_group()
+    optgrp_output.add_argument('-z', '--debug',
+                               # const=0, default='0', type=int,
+                               # nargs="?",
+                               # dest='debug',
+                               # help='debug verbosity: 0 = none, 1 = some, '
+                               #      '2 = more, 3 = everything. '
+                               #      'No number means some. '
+                               #      'Default is no debug verbosity.')
+                               dest='debug',
+                               action='store_true',
+                               help='debug mode')
+
+    optgrp_output.add_argument('-v', '--verbose',
+                               dest='verbose',
+                               action='store_true',
+                               help='verbose mode',
+                               default=False)
+
+    optgrp_output.add_argument('-q', '--quiet',
+                               dest='quiet',
+                               action='store_true',
+                               help='quiet mode',
+                               default=False)
+
+    parser.add_argument('--dump-options',
+                        dest='dump_options',
+                        action='store_true',
+                        help='dump options to stdout')
+
+    # Add option group for actions to be performed.
+    optgrp_action = parser.add_argument_group('Action options')
+    optgrp_action.add_argument('--list-datetime',
+                               dest='list_datetime',
+                               action='store_true',
+                               help='list all found date/time-information')
+
+    optgrp_action.add_argument('--list-title',
+                               dest='list_title',
+                               action='store_true',
+                               help='list all "title" information')
+
+    optgrp_action.add_argument('--list-all',
+                               dest='list_all',
+                               action='store_true',
+                               help='list all found information')
+
+    optgrp_action.add_argument('--prepend-datetime',
+                               dest='prepend_datetime',
+                               action='store_true',
+                               help='prepend most probable '
+                                    'date/time-information to file name')
+
+    optgrp_action.add_argument('--automagic',
+                               dest='automagic',
+                               action='store_true',
+                               help='Figure out most probable new filename '
+                                    'without requiring user interaction.'
+                                    ' (development feature)')
+
+    parser.add_argument(dest='input_files',
+                        metavar='filename',
+                        nargs='*',
+                        help='input file(s)')
+
+    parser.add_argument('-d', '--dry-run',
+                        dest='dry_run',
+                        action='store_true',
+                        help='simulate what would happen but do not '
+                             'actually write any changes to disk')
+
+    # Add option group for filter options.
+    optgrp_filter = parser.add_argument_group('Processing options')
+    ignore_to_year_default = str(dateandtime.YEAR_LOWER_LIMIT.strftime('%Y'))
+    optgrp_filter.add_argument('--ignore-before-year',
+                               metavar='YYYY',
+                               type=arg_is_year,
+                               default=ignore_to_year_default,
+                               nargs='?',
+                               dest='filter_ignore_to_year',
+                               action='store',
+                               help='ignore date/time-information from '
+                                    'this year and the years prior. '
+                                    'Default: {}'.format(
+                                   ignore_to_year_default))
+
+    ignore_from_year_default = str(dateandtime.YEAR_UPPER_LIMIT.strftime('%Y'))
+    optgrp_filter.add_argument('--ignore-after-year',
+                               metavar='YYYY',
+                               type=arg_is_year,
+                               default=ignore_from_year_default,
+                               nargs='?',
+                               dest='filter_ignore_from_year',
+                               action='store',
+                               help='ignore date/time-information from '
+                                    'this year onward. '
+                                    'Default: {}'.format(
+                                   ignore_from_year_default))
+
+    optgrp_filter.add_argument('--ignore-years',
+                               metavar='YYYY',
+                               type=arg_is_year,
+                               default=[],
+                               nargs='*',
+                               dest='filter_ignore_years',
+                               action='store',
+                               help='ignore date/time-information '
+                                    'from year(s)')
+    return parser
+
+
+def parse_args():
+    """
+    Parse command line arguments.
+    Check combination legality, print debug info.
+    Apply selected options.
+    """
+    parser = init_argparser()
+    args = parser.parse_args()
+
+    # Setup logging output format.
+    # if args.debug == 0:
+    if args.debug:
+        fmt = Fore.LIGHTBLACK_EX + '%(asctime)s' + Fore.RESET + \
+              Fore.LIGHTBLUE_EX + ' %(levelname)-8.8s' + Fore.RESET + \
+              ' %(funcName)-25.25s (%(lineno)3d) ' + \
+              Fore.LIGHTBLACK_EX + ' -- ' + Fore.RESET + \
+              '%(message)-120.120s'
+        logging.basicConfig(level=logging.DEBUG, format=fmt,
+                            datefmt='%Y-%m-%d %H:%M:%S')
+    # elif args.debug == 1:
+    #     # TODO: Fix debug logging verbosity.
+    #     pass
+    #
+    # elif args.debug == 2:
+    #     # TODO: Fix debug logging verbosity.
+    #     pass
+    #
+    # elif args.debug == 3:
+    #     # TODO: Fix debug logging verbosity.
+    #     pass
+
+    elif args.verbose:
+        fmt = Fore.LIGHTBLACK_EX + '%(asctime)s' + Fore.RESET + \
+              Fore.LIGHTBLUE_EX + ' %(levelname)-8.8s' + Fore.RESET + \
+              Fore.LIGHTBLACK_EX + ' -- ' + Fore.RESET + \
+              '%(message)-130.130s'
+        logging.basicConfig(level=logging.INFO, format=fmt,
+                            datefmt='%Y-%m-%d %H:%M:%S')
+    elif args.quiet:
+        fmt = '%(levelname)s -- %(message)s'
+        logging.basicConfig(level=logging.CRITICAL, format=fmt)
+    else:
+        fmt = '%(levelname)s -- %(message)s'
+        logging.basicConfig(level=logging.WARNING, format=fmt)
+
+    return args
