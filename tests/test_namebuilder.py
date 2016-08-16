7f80373b6aff0a2e7d00f7504c2c62ee524879de
Jonas Sjöberg <jomeganas@gmail.com>
2016-08-16 17:44:57 +0200

    Add unit test for class 'NameBuilder'.

---
+++ tests/test_namebuilder.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# This file is part of autonameow.
+# Copyright 2016, Jonas Sjoberg.
+
+from unittest import TestCase
+
+
+class TestNameBuilder(TestCase):
+    def test_build_name(self):
+        self.fail()
