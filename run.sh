052dd11c6e9f1b357462a53e45711f57dfc8ed70
Jonas Sjöberg <jomeganas@gmail.com>
2017-04-03 12:44:17 +0200

    Attempt at fixing launch script path resolve.
    Partial fix for resolving absolute paths in the launcher script.
    
    The MacOS (BSD) version of 'readlink' does not have the '-f' option.
    
    This change adds a test of which version of 'readlink' is available and
    a partial fix for MacOS. Still does not handle multiple layers, or
    nested symlinks properly.

--- run.sh
+++ run.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-AUTONAMEOW_PATH="$(dirname -- "$(readlink -fn -- "$0")")"
+
+# The version of readlink shipped with MacOS does not have the '-f' option.
+# Description from the "readlink (GNU coreutils) 8.25" manpage:
+#
+#   -f, --canonicalize
+#   canonicalize by following every symlink in every component of 
+#   the given name recursively; all but the last component must exist
+
+if readlink --version 2>/dev/null | grep -q 'GNU coreutils'
+then
+    # Using GNU coreutils version of readlink.
+    AUTONAMEOW_PATH="$(dirname -- "$(readlink -fn -- "$1")")"
+else
+    # Running on MacOS, resolving path with perl shim.
+
+    # TODO: This does not work properly for nested symlinks.
+    readlinkf() { perl -MCwd -e 'print Cwd::abs_path shift' "$1" ; }
+    AUTONAMEOW_PATH="$(dirname -- "$(readlinkf "$1")")"
+fi
+
 
 (cd "$AUTONAMEOW_PATH") 2>/dev/null 
 if [ "$?" -ne "0" ]
