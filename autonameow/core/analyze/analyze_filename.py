jonasjberg/autonameow

commit 71efb11c002cce1870b4de4a784a6083840dd96d

Make 'guessit' integration optional.
Makes using the 'guessit' package optional by suppressing missing
package errors. Also checks that the package is imported before
attempting to access any of its methods.

autonameow/core/analyze/analyze_filename.py
@@ -4,7 +4,10 @@
 
 import logging
 
-import guessit as guessit
+try:
+    import guessit as guessit
+except ImportError:
+    pass
 
 from core.analyze.analyze_abstract import AbstractAnalyzer
 from core.util import dateandtime
@@ -23,7 +26,7 @@ class FilenameAnalyzer(AbstractAnalyzer):
     # @Overrides method in AbstractAnalyzer
     def run(self):
         # Arbitrary length check limits (very slow) calls to guessit.
-        if len(self.file_object.filenamepart_base) > 20:
+        if len(self.file_object.filenamepart_base) > 20 and guessit:
             self.guessit_metadata = self._get_metadata_from_guessit()
 
     # @Overrides method in AbstractAnalyzer

commit 8c6a3402d0a2dddc9c70091c3cf540fa61d77ae3

Fix conditional 'guessit' integration.
Fixes bug in handling conditional integration of 'guessit' caused by
testing an uninitialized variable. Adds missing variable initialization.

Bug was introduced in commit:  71efb11c002cce1870b4de4a784a6083840dd96d

autonameow/core/analyze/analyze_filename.py
@@ -7,7 +7,7 @@ import logging
 try:
     import guessit as guessit
 except ImportError:
-    pass
+    guessit = False
 
 from core.analyze.analyze_abstract import AbstractAnalyzer
 from core.util import dateandtime
@@ -26,7 +26,7 @@ class FilenameAnalyzer(AbstractAnalyzer):
     # @Overrides method in AbstractAnalyzer
     def run(self):
         # Arbitrary length check limits (very slow) calls to guessit.
-        if len(self.file_object.filenamepart_base) > 20 and guessit:
+        if guessit and len(self.file_object.filenamepart_base) > 20:
             self.guessit_metadata = self._get_metadata_from_guessit()
 
     # @Overrides method in AbstractAnalyzer