1293f49b28ab8a1b324877469b868bfd287bc606
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-08 22:13:51 +0200

    Export log file variable 'AUTONAMEOW_TEST_LOG'.

--- tests/utils.sh
+++ tests/utils.sh
@@ -41,6 +41,7 @@ fi
 # Get absolute path to the log file, used by all sourcing scripts.
 _LOGFILE_TIMESTAMP="$(date "+%Y-%m-%dT%H%M%S")"
 AUTONAMEOW_TEST_LOG="${_LOGFILE_DIR}/${_LOGFILE_TIMESTAMP}.raw"
+export AUTONAMEOW_TEST_LOG
 if [ -f "$AUTONAMEOW_TEST_LOG" ]
 then
     # echo "NOTE: File exists: \"${AUTONAMEOW_TEST_LOG}\" .. " >&2
