4b926fdce02042c4b867da097619d4d6f335dc97
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-08 23:39:45 +0200

    Add "Test Suite" test suite 'test_tests.sh'.

---
+++ tests/test_tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+
+SELF="$(basename "$0")"
+SELF_DIR="$(dirname "$0")"
+
+source "${SELF_DIR}/utils.sh"
+
+
+
+# Test Cases
+# ____________________________________________________________________________
+
+
+logmsg "Started \"${SELF}\""
+logmsg "Running the Test Suite test suite .."
+
+
+
+assert_true  "[ "0" -eq "0" ]" 'Expect success ..'
+assert_true  "[ "1" -eq "0" ]" 'Expect failure ..'
+assert_false "[ "1" -eq "0" ]" 'Expect success ..'
+assert_false "[ "1" -ne "0" ]" 'Expect failure ..'
+
+
+
+calculate_statistics
+logmsg "Completed the Test Suite test suite tests in ${SECONDS} seconds"
