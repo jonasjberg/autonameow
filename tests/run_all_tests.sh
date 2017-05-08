5957d9e48e700b2f617aff9c0458dbe798581c79
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-08 20:53:59 +0200

    Add main test runner 'tests/run_all_tests.sh'.
    Adds a new file 'run_all_tests.sh' which is the main test runner script
    for running all tests.
    
    This script calls other "test suites" which perform the actual tests.
    All files in the 'tests' directory matching "test_*.sh" are considered
    test suites to be executed.
    
    This script keeps track of the total execution time and final conversion
    of the test output log to HTML.

---
+++ tests/run_all_tests.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# NOTE: Requires "aha" to be installed in order to convert the "raw"
+#       (containing ANSI escape codes) log files to HTML.
+
+
+SELF="$(basename "$0")"
+SELF_DIR="$(realpath -e "$(dirname "$0")")"
+
+source "${SELF_DIR}/utils.sh"
+
+
+# Store current time for later calculation of total execution time.
+# Appending %N to date +%s gives us nanosecond accuracy.
+time_start="$(date +%s%N)"
+
+logmsg "Started \"${SELF}\""
+logmsg "Executing all files in \"${SELF_DIR}\" matching \"test_*.sh\".."
+
+
+find "$SELF_DIR" -mindepth 1 -maxdepth 1 -type f -name "test_*.sh" \
+| while IFS='\n' read -r testscript
+do
+    if [ ! -x "$testscript" ]
+    then
+        logmsg "Missing execute permission: \"${testscript}\" .. SKIPPING"
+        continue
+    fi
+
+    "$testscript"
+done
+
+
+total_time="$((($(date +%s%N) - $time_start) / 1000000))"
+logmsg "Total executing time: ${total_time} ms"
+
+
+
+if command -v "aha" >/dev/null 2>&1
+then
+    [ -z "${AUTONAMEOW_TEST_LOG:-}" ] && exit 1
+    [ -f "$AUTONAMEOW_TEST_LOG" ] || exit 1
+
+    _html_test_log="${AUTONAMEOW_TEST_LOG%.*}.html"
+
+    if aha < "$AUTONAMEOW_TEST_LOG" | sed 's///g' > "$_html_test_log"
+    then
+        if [ -f "$_html_test_log" ]
+        then
+            logmsg "Wrote HTML log file: \"${_html_test_log}\""
+            rm -- "$AUTONAMEOW_TEST_LOG"
+        fi
+    else
+        logmsg 'FAILED to write HTML log file!'
+    fi
+else
+    logmsg "The executable \"aha\" is not available on this system"
+    logmsg "Skipping converting raw logfiles to HTML .."
+    exit 1
+fi
+
+
