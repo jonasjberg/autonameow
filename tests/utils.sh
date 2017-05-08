95ef07087a91d6be1f928095428e787cba4cbc1f
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-08 14:17:25 +0200

    Add common testing library 'utils.sh'.
    Adds a new file 'tests/utils.sh' with various testing utilities.
    
    Handles logging, messages, assertions and statistics.
    Should be sourced by individual test suites and scripts that perform
    testing.

---
+++ tests/utils.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+
+C_RED="$(tput setaf 1)"
+C_GREEN="$(tput setaf 2)"
+C_RESET="$(tput sgr0)"
+
+tests_total=0
+tests_passed=0
+tests_failed=0
+
+
+logmsg()
+{
+    local _timestamp="$(date "+%Y-%m-%d %H:%M:%S")"
+    printf "%s %s\n" "$_timestamp" "$*"
+}
+
+logresults()
+{
+    if [ "$tests_failed" -eq "0" ]
+    then
+        printf "\n${C_GREEN}[ ALL TESTS PASSED ]${C_RESET}"
+    else
+        printf "\n${C_RED}[ TESTS FAILED ]${C_RESET}"
+    fi
+
+    printf " Executed %d tests -- Pass: %d   Fail: %d\n\n" "$tests_total" "$tests_passed" "$tests_failed"
+}
+
+test_fail()
+{
+    printf "${C_RED}[ TEST FAILED ]${C_RESET} %s\n" "$*"
+    tests_failed="$((tests_failed + 1))"
+    tests_total="$((tests_total + 1))"
+}
+
+test_pass()
+{
+    printf "${C_GREEN}[ TEST PASSED ]${C_RESET} %s\n" "$*"
+    tests_passed="$((tests_passed + 1))"
+    tests_total="$((tests_total + 1))"
+}
+
+assert_true()
+{
+    eval "${1}"
+    if [ "$?" -ne "0" ]
+    then
+        shift ; test_fail "${FUNCNAME[1]}  $*"
+    else
+        shift ; test_pass "${FUNCNAME[1]}  $*"
+    fi
+}
+
+assert_false()
+{
+    eval "${1}"
+    if [ "$?" -ne "0" ]
+    then
+        shift ; test_pass "${FUNCNAME[1]}  $*"
+    else
+        shift ; test_fail "${FUNCNAME[1]}  $*"
+    fi
+}
+
+
+
+# Test Cases
+# ____________________________________________________________________________
+#
+# Detect if this script is being sourced by another script.
+# Check Bash version for support:   man bash | less -p BASH_SOURCE
+# Source:  http://stackoverflow.com/a/2684300/7802196
+
+[[ ${BASH_VERSINFO[0]} -le 2 ]] && { echo 'No BASH_SOURCE array variable' 1>&2 ; exit 1 ; }
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] # && echo "script ${BASH_SOURCE[0]} is being sourced ..."
+
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+    logmsg "Starting self-tests .."
+    assert_true  "[ "0" -eq "0" ]" '(Internal Test) Expect success ..'
+    assert_true  "[ "1" -eq "0" ]" '(Internal Test) Expect failure ..'
+    assert_false "[ "1" -eq "0" ]" '(Internal Test) Expect success ..'
+    assert_false "[ "1" -ne "0" ]" '(Internal Test) Expect failure ..'
+    logmsg "Finished self-tests!"
+fi
+
