8b78973f9fc437b1373a08029efcc2e898bc2f28
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-08 14:21:10 +0200

    Add documentation test suite 'test_docs.sh'.
    Adds a new file 'test_docs.sh' with unit tests for documentation.
    
    Depends on 'utils.sh' that must be located in the same directory.

---
+++ tests/test_docs.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+
+SELF="$(basename "$0")"
+SELF_DIR="$(dirname "$0")"
+
+source "${SELF_DIR}/utils.sh"
+
+
+
+# Test Cases
+# ____________________________________________________________________________
+
+
+logmsg "Starting documentation tests \"${SELF}\""
+
+
+
+FORMATS_DOC="$( ( cd "$SELF_DIR" && realpath -e "../docs/formats.md" ) )"
+assert_true '[ -f "$FORMATS_DOC" ]' "Data formats docs \""$(basename -- "$FORMATS_DOC")"\" should exist"
+
+assert_true '[ "$(cat "$FORMATS_DOC" | wc -l)" -gt "50" ]' "Data formats docs must have some content"
+
+assert_false 'grep -q "\(TODO\|FIXME\|XXX\).*" "$FORMATS_DOC"' "Data formats docs should not contain TODOs"
+
+
+
+logmsg "Finished documentation tests!"
+
+logresults
