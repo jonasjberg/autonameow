df9f611b6c340e52eb88c9476f81d733d631cc8f
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-15 13:05:22 +0200

    Add new integration test suite, "Source Code".
    Adds a new integration test suite "Source Code test suite".
    
    Contains a new test case;
    "[TC007] the analysis class should not perform results printing".
    
    This test is probably technically not an integration test, but it is
    easier to add the test here then as a unit test.

---
+++ tests/integration_test_src.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+set -o noclobber -o nounset -o pipefail
+
+SELF="$(basename "$0")"
+SELF_DIR="$(dirname "$0")"
+
+if ! source "${SELF_DIR}/integration_utils.sh"
+then
+    echo "Integration test utility library is missing. Aborting .." 1>&2
+    exit 1
+fi
+
+
+
+# Test Cases
+# ____________________________________________________________________________
+
+# Store current time for later calculation of total execution time.
+time_start="$(current_unix_time)"
+
+logmsg "Started \"${SELF}\""
+logmsg "Running the Source Code test suite .."
+
+
+
+ANALYSIS_RESULTS="$( ( cd "$SELF_DIR" && realpath -e "../autonameow/core/analyze/analysis.py" ) )"
+assert_true '[ -f "$ANALYSIS_RESULTS" ]' \
+            'The source file containing the "analysis" class exists'
+assert_false 'grep -qE -- "def print.*\(" "$ANALYSIS_RESULTS"' \
+             "[TC007] The analysis class should not perform results printing"
+
+
+
+# Calculate total execution time.
+time_end="$(current_unix_time)"
+total_time="$((($time_end - $time_start) / 1000000))"
+
+calculate_statistics
+logmsg "Completed the Source Code test suite tests in ${total_time} ms"
