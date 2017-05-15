4d84bf7a9ec28517d3f6d0a1e4aa3f2da394c0fe
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-15 18:07:04 +0200

    Various fixes to 'common_runner.sh'.

--- tests/common_runner.sh
+++ tests/common_runner.sh
@@ -52,6 +52,13 @@ else
     count_fail="$((count_fail + 1))"
 fi
 
+if [ "$count_fail" -ne "0" ]
+then
+    echo "An error occurred; Aborting .." 1>&2
+    exit 1
+fi
+
+
 
 wiki_report_results="${AUTONAMEOW_WIKI_ROOT_DIR}/Test-Results.md"
 report_append()
@@ -59,48 +66,51 @@ report_append()
     printf "$*" >> "$wiki_report_results"
 }
 
-if [ "$count_fail" -eq "0" ]
+
+# Insert heading with todays date if not already present.
+_date="$(date "+%Y-%m-%d")"
+if ! grep -q "^### ${_date}$" "$wiki_report_results"
 then
-    # Insert heading with todays date if not already present.
-    _date="$(date "+%Y-%m-%d")"
-    if ! grep -q "^### ${_date}$" "$wiki_report_results"
-    then
-        report_append "\n### ${_date}\n\n"
-    fi
-
-    # Root path used in the links that are added to the report.
-    REMOTE_TEST_RESULTS="https://github.com/1dv430/js224eh-project/blob/master/docs/test_results"
-
-    if [ -f "${AUTONAMEOW_TESTRESULTS_DIR}/.integrationlog.toreport" ]
-    then
-        # Read file contents; the path to the most recent integration test log.
-        _int_log_path="$( < "${AUTONAMEOW_TESTRESULTS_DIR}/.integrationlog.toreport" )"
-        if [ ! -f "$_int_log_path" ]
-        then
-            echo "Not a file: \"${_int_log_path}\". Aborting .." 1>&2
-            exit 1
-        fi
-
-        _int_log_basename="$(basename -- "${_int_log_path}")"
-        _int_log_timestamp="$(get_timestamp_from_basename "${_int_log_basename}")"
-        _int_log_link="${REMOTE_TEST_RESULTS}/${_int_log_basename}"
-        report_append "* \`${_int_log_timestamp}\` [Integration Test Report](${_int_log_link})\n"
-    fi
-
-    if [ -f "${AUTONAMEOW_TESTRESULTS_DIR}/.unittestlog.toreport" ]
-    then
-        # Read file contents; the path to the most recent unit test log.
-        _unit_log_path="$( < "${AUTONAMEOW_TESTRESULTS_DIR}/.unittestlog.toreport" )"
-        if [ ! -f "$_unit_log_path" ]
-        then
-            echo "Not a file: \"${_unit_log_path}\". Aborting .." 1>&2
-            exit 1
-        fi
-
-        _unit_log_basename="$(basename -- "${_unit_log_path}")"
-        _unit_log_timestamp="$(get_timestamp_from_basename "${_unit_log_basename}")"
-        _unit_log_link="${REMOTE_TEST_RESULTS}/${_unit_log_basename}"
-        report_append "* \`${_unit_log_timestamp}\` [Unit Test Report](${_unit_log_link})\n"
-    fi
+    report_append "\n### ${_date}\n\n"
 fi
 
+# Root path used in the links that are added to the report.
+REMOTE_TEST_RESULTS="https://github.com/1dv430/js224eh-project/blob/master/docs/test_results"
+
+if [ -f "${AUTONAMEOW_TESTRESULTS_DIR}/.integrationlog.toreport" ]
+then
+    # Read file contents; the path to the most recent integration test log.
+    _int_log_path="$( < "${AUTONAMEOW_TESTRESULTS_DIR}/.integrationlog.toreport" )"
+    if [ ! -f "$_int_log_path" ]
+    then
+        echo "Not a file: \"${_int_log_path}\". Aborting .." 1>&2
+        exit 1
+    fi
+
+    printf "Adding integration test log to Test Results wiki page .."
+    _int_log_basename="$(basename -- "${_int_log_path}")"
+    _int_log_timestamp="$(get_timestamp_from_basename "${_int_log_basename}")"
+    _int_log_link="${REMOTE_TEST_RESULTS}/${_int_log_basename}"
+    report_append "* \`${_int_log_timestamp}\` [Integration Test Report](${_int_log_link})\n"
+    printf " [FINISHED]\n"
+fi
+
+if [ -f "${AUTONAMEOW_TESTRESULTS_DIR}/.unittestlog.toreport" ]
+then
+    # Read file contents; the path to the most recent unit test log.
+    _unit_log_path="$( < "${AUTONAMEOW_TESTRESULTS_DIR}/.unittestlog.toreport" )"
+    if [ ! -f "$_unit_log_path" ]
+    then
+        echo "Not a file: \"${_unit_log_path}\". Aborting .." 1>&2
+        exit 1
+    fi
+
+    printf "Adding unit test log to Test Results wiki page .."
+    _unit_log_basename="$(basename -- "${_unit_log_path}")"
+    _unit_log_timestamp="$(get_timestamp_from_basename "${_unit_log_basename}")"
+    _unit_log_link="${REMOTE_TEST_RESULTS}/${_unit_log_basename}"
+    report_append "* \`${_unit_log_timestamp}\` [Unit Test Report](${_unit_log_link})\n"
+    printf " [FINISHED]\n"
+fi
+
+printf "\nCompleted in $SECONDS seconds\n"
