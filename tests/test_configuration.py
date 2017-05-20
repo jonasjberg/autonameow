a1cace99e8116f477b58745b1c0823c290626742
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-20 03:59:13 +0200

    Add assertion failure messages and comments.

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -84,20 +84,22 @@ class TestDefaultConfig(TestCase):
         self.configuration = Configuration()
         self.configuration.load_from_dict(NEW_DEFAULT_CONFIG)
 
-    def default_configuration_exists(self):
-        self.assertIsNotNone(NEW_DEFAULT_CONFIG)
+    def test_default_configuration_exists(self):
+        self.assertIsNotNone(NEW_DEFAULT_CONFIG,
+                             'Default config dict is available')
 
-    def default_configuration_contain_rules(self):
-        self.assertIsNotNone(self.configuration.data.rules)
+    def test_default_configuration_contain_rules(self):
+        self.assertIsNotNone(self.configuration.rules)
 
-    def default_configuration_rules_is_type_list(self):
-        self.assertEqual(type)
-
-    def default_configuration_contain_at_least_two_rules(self):
-        self.assertGreaterEqual(2, len(self.configuration.data.rules))
+    def test_default_configuration_contain_at_least_two_rules(self):
+        self.assertGreaterEqual(len(self.configuration.rules), 2,
+                                'Arbitrary rule count test')
 
 
 class TestWriteDefaultConfig(TestCase):
+    # Show contents of most recent temporary log on Mac OS;
+    # cat "$(find /var/folders -type f -name "test_default_config.yaml" -exec stat -f %m %N {} \; 2>/dev/null | sort -n | cut -f2 -d  | tail -n 1)"
+
     def setUp(self):
         self.dest_path = os.path.join(make_temp_dir(),
                                       'test_default_config.yaml')
@@ -110,7 +112,8 @@ class TestWriteDefaultConfig(TestCase):
 
     def test_load_default_config_from_dict_before_write(self):
         self.configuration.load_from_dict(NEW_DEFAULT_CONFIG)
-        self.assertIsNotNone(self.configuration.data)
+        self.assertIsNotNone(self.configuration.data,
+                             'Configuration data should exist')
 
     def test_write_default_config_to_disk(self):
         self.configuration.load_from_dict(NEW_DEFAULT_CONFIG)
