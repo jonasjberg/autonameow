76937a5b8dcf87aee6f640cc045e145651bb8094
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-21 18:55:12 +0200

    Modify (WIP) default configuration.

--- autonameow/core/config_defaults.py
+++ autonameow/core/config_defaults.py
@@ -147,6 +147,16 @@ NEW_DEFAULT_CONFIG = [
 ]
 
 NEW_DEFAULT_CONFIG_2 = {
+    # File rules specify conditions that should be true for the rule to apply
+    # for a given file.
+    #
+    # If '_exact_match' is True, *all* conditions must apply for the rule to be
+    # considered a match for a given file.
+    # Otherwise, when '_exact_match' is False, the rule with the most amount of
+    # satisfied conditions is chosen.
+    # If multiple rules end up tied (same amount of satisified conditions for a
+    # given file) the '_weight' is used as a last resort to pick out a single
+    # rule to use.
     'file_rules': [
         {'_description': 'First Entry in the Default Configuration',
          '_exact_match': False,
@@ -174,7 +184,7 @@ NEW_DEFAULT_CONFIG_2 = {
         {'_description': 'Sample Entry for Photos with strict rules',
          '_exact_match': True,
          '_weight': 1,
-         'name_template': '[datetime(%Y-%m-%dT%H%M%S)] [description] -- [tags].[ext]',
+         'name_format': '%(datetime(%Y-%m-%dT%H%M%S))s %(description)s -- %(tags)s.%(extension)s',
          'conditions': {
              'filename': {
                  'pathname': '~/Pictures/incoming',
@@ -198,38 +208,71 @@ NEW_DEFAULT_CONFIG_2 = {
              'tags': 'plugin.microsoftvision.tags'
          }
          },
-        {'_description': 'Sample Entry for Photos with strict rules',
+        {'_description': 'Sample Entry for EPUB e-books',
          '_exact_match': True,
          '_weight': 1,
-         'name_template': '[datetime(%Y-%m-%dT%H%M%S)] [description] -- [tags].[ext]',
+         'name_format': '',
+         'name_template': 'default_ebook_name',
          'conditions': {
              'filename': {
-                 'pathname': '~/Pictures/incoming',
-                 'basename': 'DCIM*',
-                 'extension': 'jpg'
+                 'pathname': None,
+                 'basename': None,
+                 'extension': 'epub'
              },
              'contents': {
-                 'mime_type': 'image/jpeg',
-                 'metadata': 'exif.datetimeoriginal'
+                 'mime_type': 'application/epub+zip',
+                 'metadata': 'metadata.XMP-dc.***'
              }
          },
          'data_sources': {
-             'datetime': ['metadata.exif.datetimeoriginal',
-                          'metadata.exif.datetimedigitized',
-                          'metadata.exif.createdate'],
-             'description': 'plugin.microsoftvision.caption',
-             'title': None,
-             'author': None,
-             'publisher': None,
+             'datetime': ['metadata.XMP-dc.PublicationDate',
+                          'metadata.XMP-dc.Date'],
+             'description': None,
+             'title': 'metadata.XMP-dc.Title',
+             'author': ['metadata.XMP-dc.Creator',
+                        'metadata.XMP-dc.CreatorFile-as'],
+             'publisher': 'metadata.XMP-dc.Publisher',
+             'edition': None,
              'extension': 'filename.extension',
-             'tags': 'plugin.microsoftvision.tags'
+             'tags': None
+         }
+         },
+        {'_description': 'Sample Entry for MOBI e-books',
+         '_exact_match': True,
+         '_weight': 1,
+         'name_format': None,
+         'name_template': 'default_ebook_name',
+         'conditions': {
+             'filename': {
+                 'pathname': None,
+                 'basename': None,
+                 'extension': ['mobi']
+             },
+             'contents': {
+                 'mime_type': 'application/x-mobipocket-ebook',
+                 'metadata': 'metadata.MOBI.***'
+             }
+         },
+         'data_sources': {
+             'datetime': ['metadata.MOBI.PublishDate'],
+             'description': ['metadata.MOBI.Description',
+                             'metadata.MOBI.Subject'],
+             'title': ['metadata.MOBI.BookName',
+                       'metadata.MOBI.UpdatedTitle'],
+             'author': 'metadata.MOBI.Author',
+             'publisher': 'metadata.MOBI.Publisher',
+             'extension': 'filename.extension',
+             'tags': None
          }
          }
     ],
     'name_templates': [
         {'_description': 'First name template in the Default Configuration',
          '_name': 'default_template_name',
-         'name_format': '%(title)s - %(author)s %(datetime)s.%(extension)s'}
+         'name_format': '%(title)s - %(author)s %(datetime)s.%(extension)s'},
+        {'_description': 'Ebook name template in the Default Configuration',
+         '_name': 'default_ebook_name',
+         'name_format': '%(publisher)s %(title)s %(edition)s - %(author)s %(year)s.%(extension)s'}
     ]
 }
 
