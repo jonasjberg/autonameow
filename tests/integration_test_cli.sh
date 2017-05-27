6bffc0af0e474fbd66b163ae629db3d1b60c537f
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-28 00:12:09 +0200

    Add integration tests for IDs [TC011] and [TC010].

--- tests/integration_test_cli.sh
+++ tests/integration_test_cli.sh
@@ -75,6 +75,26 @@ assert_true '"$AUTONAMEOW_RUNNER" --help | grep -q -- "dry-run"' \
 assert_true '( "$AUTONAMEOW_RUNNER" --interactive 2>&1 ) >/dev/null' \
             "[TC013] autonameow should provide a \"--interactive\" option"
 
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic 2>&1 ) >/dev/null' \
+             "autonameow should return non-zero when started with \"--automagic\" without specifying files"
+
+
+SAMPLE_JPG_FILE="$( ( cd "$SELF_DIR" && realpath -e "../test_files/smulan.jpg" ) )"
+assert_true '[ -e "$SAMPLE_JPG_FILE" ]' \
+            "The test sample jpg file exists. Add suitable test file if this test fails!"
+
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run -- "$SAMPLE_JPG_FILE" 2>&1 ) >/dev/null' \
+             "[TC011][TC001] autonameow should return zero when started with \"--automagic\", \"--dry-run\" and a valid file"
+
+_expected_name="/tmp/2010-01-31T161251 a cat lying on a rug.jpg"
+assert_true 'cp -n -- "$SAMPLE_JPG_FILE" /tmp/autonameow_sample 2>&1 >/dev/null' \
+            "[rename_sample_jpg_file] Test setup should succeed"
+
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run -- /tmp/autonameow_sample 2>&1 ) >/dev/null && [ -f "$_expected_name" ]' \
+            "[rename_sample_jpg_file] [TC010][TC011] \""$(basename -- "${SAMPLE_JPG_FILE}")"\" should be renamed to \"${_expected_name}\""
+
+assert_true '[ -f "/tmp/autonameow_sample" ] && rm -- "/tmp/autonameow_sample" ; [ -f "$_expected_name" ] && rm -- "$_expected_name" || true' \
+            "[rename_sample_jpg_file] Test teardown should succeed"
 
 
 # Calculate total execution time.
