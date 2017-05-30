c66e2c7fe6dd313c57162a3e648be7daa2304b72
Jonas Sjöberg <jomeganas@gmail.com>
2017-05-30 05:56:49 +0200

    Fix and add tests to Integration Test Suite.
    Fixes failing tests output not suppressed properly.
    
    Also adds additional test cases.

--- tests/integration_test_cli.sh
+++ tests/integration_test_cli.sh
@@ -60,23 +60,38 @@ assert_true '[ -e "$AUTONAMEOW_RUNNER" ]' \
 assert_true '[ -x "$AUTONAMEOW_RUNNER" ]' \
             "The autonameow launcher script has executable permission"
 
-assert_true '"$AUTONAMEOW_RUNNER" >/dev/null' \
+assert_true '( "$AUTONAMEOW_RUNNER" 2>&1 ) >/dev/null' \
             "The autonameow launcher script can be started with no arguments"
 
-assert_true '"$AUTONAMEOW_RUNNER" | grep -q -- "--help"' \
+assert_true '( "$AUTONAMEOW_RUNNER" 2>&1 | grep -q -- "--help" ) >/dev/null' \
             "[TC005] autonameow should print how to get help when started with no arguments"
 
-assert_true '"$AUTONAMEOW_RUNNER" --help | head -n 1 | grep -q -- "usage"' \
+assert_true '( "$AUTONAMEOW_RUNNER" --help -- 2>&1 | head -n 1 | grep -q -- "usage" ) >/dev/null' \
             "[TC005] autonameow should display usage information when started with \"--help\""
 
-assert_true '"$AUTONAMEOW_RUNNER" --help | grep -q -- "dry-run"' \
+assert_true '( "$AUTONAMEOW_RUNNER" --help -- 2>&1 | grep -q -- "dry-run" ) >/dev/null' \
             "[TC001] autonameow should provide a \"--dry-run\" option"
 
-assert_true '( "$AUTONAMEOW_RUNNER" --interactive 2>&1 ) >/dev/null' \
+assert_true '( "$AUTONAMEOW_RUNNER" --help -- 2>&1 | grep -q -- "--interactive" ) >/dev/null' \
             "[TC013] autonameow should provide a \"--interactive\" option"
 
-assert_false '( "$AUTONAMEOW_RUNNER" --automagic 2>&1 ) >/dev/null' \
+assert_false '( "$AUTONAMEOW_RUNNER" --interactive -- 2>&1 ) >/dev/null' \
+             "[TC013] autonameow should return non-zero when started with \"--interactive\" without specifying files"
+
+assert_false '( "$AUTONAMEOW_RUNNER" --interactive --verbose -- 2>&1 ) >/dev/null' \
+             "[TC013] autonameow should return non-zero when started with \"--interactive\" and \"--verbose\" without specifying files"
+
+assert_false '( "$AUTONAMEOW_RUNNER" --interactive --debug -- 2>&1 ) >/dev/null' \
+             "[TC013] autonameow should return non-zero when started with \"--interactive\" and \"--debug\" without specifying files"
+
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic -- 2>&1 ) >/dev/null' \
              "autonameow should return non-zero when started with \"--automagic\" without specifying files"
+
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic --verbose -- 2>&1 ) >/dev/null' \
+             "autonameow should return non-zero when started with \"--automagic\" and \"--verbose\" without specifying files"
+
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic --debug -- 2>&1 ) >/dev/null' \
+             "autonameow should return non-zero when started with \"--automagic\" and \"--debug\" without specifying files"
 
 
 SAMPLE_JPG_FILE="$( ( cd "$SELF_DIR" && realpath -e "../test_files/smulan.jpg" ) )"
@@ -86,25 +101,32 @@ assert_true '[ -e "$SAMPLE_JPG_FILE" ]' \
 assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run -- "$SAMPLE_JPG_FILE" 2>&1 ) >/dev/null' \
              "[TC011][TC001] autonameow should return zero when started with \"--automagic\", \"--dry-run\" and a valid file"
 
-_expected_name="/tmp/2010-01-31T161251 a cat lying on a rug.jpg"
-assert_true 'cp -n -- "$SAMPLE_JPG_FILE" /tmp/autonameow_sample 2>&1 >/dev/null' \
-            "[rename_sample_jpg_file] Test setup should succeed"
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_JPG_FILE" 2>&1 ) >/dev/null' \
+             "[TC011][TC001] autonameow should return zero when started with \"--automagic\", \"--dry-run\", \"--verbose\" and a valid file"
 
-assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run -- /tmp/autonameow_sample 2>&1 ) >/dev/null && [ -f "$_expected_name" ]' \
-            "[rename_sample_jpg_file] [TC010][TC011] \""$(basename -- "${SAMPLE_JPG_FILE}")"\" should be renamed to \"${_expected_name}\""
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --debug -- "$SAMPLE_JPG_FILE" 2>&1 ) >/dev/null' \
+             "[TC011][TC001] autonameow should return zero when started with \"--automagic\", \"--dry-run\", \"--debug\" and a valid file"
+
+
+_expected_name='/tmp/2010-01-31T161251 a cat lying on a rug.jpg'
+assert_true 'cp -n -- "$SAMPLE_JPG_FILE" /tmp/autonameow_sample 2>&1 >/dev/null' \
+            "rename_sample_jpg_file Test setup should succeed"
+
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run -- /tmp/autonameow_sample 2>&1 ) >/dev/null && [ -f "$_expected_name" ]' \
+            "rename_sample_jpg_file [TC010][TC011] \""$(basename -- "${SAMPLE_JPG_FILE}")"\" should be renamed to \"${_expected_name}\""
 
 assert_true '[ -f "/tmp/autonameow_sample" ] && rm -- "/tmp/autonameow_sample" ; [ -f "$_expected_name" ] && rm -- "$_expected_name" || true' \
-            "[rename_sample_jpg_file] Test teardown should succeed"
+            "rename_sample_jpg_file Test teardown should succeed"
 
 
 assert_true '( "$AUTONAMEOW_RUNNER" --version 2>&1 ) >/dev/null' \
             "autonameow should return zero when started with \"--version\""
 
 assert_true '( "$AUTONAMEOW_RUNNER" --version --verbose 2>&1 ) >/dev/null' \
-            "autonameow should return zero when started with \"--version\" and \"--verbose\""
+             "autonameow should return zero when started with \"--version\" and \"--verbose\""
 
 assert_true '( "$AUTONAMEOW_RUNNER" --version --debug 2>&1 ) >/dev/null' \
-            "autonameow should return zero when started with \"--version\" and \"--debug\""
+             "autonameow should return zero when started with \"--version\" and \"--debug\""
 
 
 
