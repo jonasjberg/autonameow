4597e76531ce9ef29d440358708e1b13e0a3fa52
Jonas Sjöberg <jomeganas@gmail.com>
2017-06-20 16:47:36 +0200

    Update tests with changes to output format.
    Updates tests in the "Command-Line Interface" test suite.
    
    Information on which file rule is used now quotes the active rule.

--- tests/integration_test_cli.sh
+++ tests/integration_test_cli.sh
@@ -155,16 +155,16 @@ assert_true '[ -e "$SAMPLE_PDF_FILE" ]' \
             "The test sample pdf file exists. Add suitable test file if this test fails!"
 
 set +o pipefail
-assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_PDF_FILE" 2>&1 | grep -q -- "Using file rule: test_files Gmail print-to-pdf" ) >/dev/null' \
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_PDF_FILE" 2>&1 | grep -q -- "Using file rule: \"test_files Gmail print-to-pdf\"" ) >/dev/null' \
             "[TC014] autonameow should choose file rule \"test_files Gmail print-to-pdf\" given the file \""$(basename -- "${SAMPLE_PDF_FILE}")"\""
 
-assert_false '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_JPG_FILE" 2>&1 | grep -q -- "Using file rule: test_files Gmail print-to-pdf" ) >/dev/null' \
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_JPG_FILE" 2>&1 | grep -q -- "Using file rule: \"test_files Gmail print-to-pdf\"" ) >/dev/null' \
              "[TC014] autonameow should NOT choose file rule \"test_files Gmail print-to-pdf\" given the file \""$(basename -- "${SAMPLE_JPG_FILE}")"\""
 
-assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_JPG_FILE" 2>&1 | grep -q -- "Using file rule: test_files smulan.jpg" ) >/dev/null' \
+assert_true '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_JPG_FILE" 2>&1 | grep -q -- "Using file rule: \"test_files smulan.jpg\"" ) >/dev/null' \
             "[TC014] autonameow should choose file rule \"test_files smulan.jpg\" given the file \""$(basename -- "${SAMPLE_JPG_FILE}")"\""
 
-assert_false '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_PDF_FILE" 2>&1 | grep -q -- "Using file rule: test_files smulan.jpg" ) >/dev/null' \
+assert_false '( "$AUTONAMEOW_RUNNER" --automagic --dry-run --verbose -- "$SAMPLE_PDF_FILE" 2>&1 | grep -q -- "Using file rule: \"test_files smulan.jpg\"" ) >/dev/null' \
              "[TC014] autonameow should NOT choose file rule \"test_files smulan.jpg\" given the file \""$(basename -- "${SAMPLE_PDF_FILE}")"\""
 set -o pipefail
 
