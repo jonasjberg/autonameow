c6c61a6a84a0f3bb3f2bd023098daa9a11aae13c
Jonas Sjöberg <jomeganas@gmail.com>
2017-06-25 19:45:31 +0200

    Update unit tests for the 'PdfTextExtractor' class.

--- tests/unit_test_extractor_textual.py
+++ tests/unit_test_extractor_textual.py
@@ -91,6 +91,7 @@ Test test. This file contains no digits whatsoever.
 
 '''
 
+
 class TestSetup(TestCase):
     def test_sample_pdf_file_exists(self):
         self.assertTrue(os.path.isfile(pdf_file))
@@ -138,6 +139,12 @@ class TestPdfTextExtractor(TestCase):
         test_file = abspath_testfile('gmail.pdf')
         self.e = PdfTextExtractor(test_file)
 
+        class DummyFileObject(object):
+            def __init__(self, mime):
+                self.mime_type = mime
+        self.fo_image = DummyFileObject(mime='image/jpeg')
+        self.fo_pdf = DummyFileObject(mime='application/pdf')
+
         self.EXPECT_TEXT = b'1/11/2016\n\nGmail - V\xc3\xa4lkommen till kursen Introduktion till Linux och sm\xc3\xa5 n\xc3\xa4tverk!\n\nJonas Sj\xc3\xb6berg <jomeganas@gmail.com>\n\nV\xc3\xa4lkommen till kursen Introduktion till Linux och sm\xc3\xa5 n\xc3\xa4tverk!\n1 message\nCamilla Nordin <Camilla.Nordin@hig.se>\nTo: Camilla Nordin <Camilla.Nordin@hig.se>\n\nFri, Jan 8, 2016 at 3:50 PM\n\nV\xc3\xa4lkommen till H\xc3\xb6gskolan i G\xc3\xa4vle och kursen Introduktion till Linux och sm\xc3\xa5 n\xc3\xa4tverk\n7,5 hp!\n\nDitt v\xc3\xa4lkomstbrev hittar du h\xc3\xa4r: http://www.hig.se/Ext/Sv/Student/Ny\xc2\xadstudent/Valkomstbrev/Kurser/\nDatateknik.html\n\nL\xc3\x84S DITT V\xc3\x84LKOMSTBREV NOGGRANT!\n\nKursen b\xc3\xb6rjar den 25:e januari men \xc3\xa4r \xc3\xb6ppen f\xc3\xb6r webbregistrering via Studentportalen fr\xc3\xa5n och\nmed den 18:e januari, se v\xc3\xa4lkomstbrevet.\n\n\xc3\x85tkomstkoden som i vissa fall beh\xc3\xb6vs f\xc3\xb6r inskrivning p\xc3\xa5 kursen i Blackboard \xc3\xa4r: Debian\xc2\xadMint\n\nKursinstansen i Blackboard \xc3\xb6ppnar den 25:e januari. Observera att du ibland m\xc3\xa5ste s\xc3\xb6ka fram\nkursen den f\xc3\xb6rsta g\xc3\xa5ngen du loggar in i Blackboard. F\xc3\xb6lj instruktionerna i manualen som finns\nl\xc3\xa4nkad i v\xc3\xa4lkomstbrevet.\n\nDet finns bra information f\xc3\xb6r nya studenter p\xc3\xa5 v\xc3\xa5r hemsida: www.hig.se/nystudent\n\nDu beh\xc3\xb6ver inte tacka ja eller nej till kursen utan accepterar din plats genom att registrera dig\nvia Studentportalen.\n\nLycka till med studierna!\n\nH\xc3\xa4lsningar Camilla\n***************************************************************\nCamilla Nordin\nH\xc3\xb6gskolan i G\xc3\xa4vle\nAkademin f\xc3\xb6r teknik och milj\xc3\xb6\nhttps://mail.google.com/mail/u/0/?ui=2&ik=dbcc4dc2ed&view=pt&q=ny%20student&qs=true&search=query&th=15221b790b7df\xe2\x80\xa6\n\n1/2\n\n1/11/2016\n\nGmail - V\xc3\xa4lkommen till kursen Introduktion till Linux och sm\xc3\xa5 n\xc3\xa4tverk!\n\n801 76 G\xc3\x84VLE\nTel: 026\xc2\xad64 87 46\nFax: 026\xc2\xad64 87 58\ne\xc2\xadmail: cnn@hig.se\nBes\xc3\xb6ksadress: Kungsb\xc3\xa4cksv\xc3\xa4gen 47, rum 12:208\n\nH\xc3\xb6gskolan i G\xc3\xa4vle, 801 76 G\xc3\xa4vle \xe2\x80\xa2 026 64 85 00 \xe2\x80\xa2 www.hig.se\nF\xc3\xb6r en h\xc3\xa5llbar livsmilj\xc3\xb6 f\xc3\xb6r m\xc3\xa4nniskan\nUniversity of G\xc3\xa4vle, SE\xc2\xad801 76 G\xc3\xa4vle, Sweden \xe2\x80\xa2 +46 (0) 26 64 85 00 \xe2\x80\xa2 www.hig.se\n\nhttps://mail.google.com/mail/u/0/?ui=2&ik=dbcc4dc2ed&view=pt&q=ny%20student&qs=true&search=query&th=15221b790b7df\xe2\x80\xa6\n\n2/2\n\n'.decode()
 
     def test_method_query_returns_something(self):
@@ -153,6 +160,14 @@ class TestPdfTextExtractor(TestCase):
     def test_method_query_arbitrary_field_result_contains_expected(self):
         actual = self.e.query('dummy_field')
         self.assertEqual(self.EXPECT_TEXT, actual)
+
+    def test_class_method_can_handle_is_defined(self):
+        self.assertIsNotNone(self.e.can_handle)
+
+    def test_class_method_can_handle_returns_expected(self):
+        self.assertFalse(self.e.can_handle(self.fo_image))
+        self.assertTrue(self.e.can_handle(self.fo_pdf))
+
 
 
 class TestImageOCRTextExtractorWithEmptyFile(TestCase):
