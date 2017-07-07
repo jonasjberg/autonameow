f0f7e5e1855c452077b798eaabd552b6911f20b1
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-07 15:40:02 +0200

    Add (WIP) 'RuleCondition' class.
    Adds a new class 'RuleCondition'.
    
    Represents a single condition contained in a configuration file rule.
    Should perform validation of incoming arguments and simplify the in
    progress refactoring of rule evaluation and related.

---
+++ autonameow/core/config/conditions.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+from core import util
+from core.config import field_parsers
+
+
+class RuleCondition(object):
+    """
+    Represents a single condition contained in a configuration file rule.
+    """
+    def __init__(self, raw_query_string, raw_expression):
+        """
+        Validates the arguments and returns a 'RuleCondition' instance if
+        the validation is successful. Else an exception is thrown.
+
+        The "query string" is a reference to some data.
+        The expression is some expression that will be evaluated on the data
+        contained at the location referenced to by the "query string".
+
+        Example: If the "query string" is 'contents.mime_type', a valid
+        expression could be 'image/*'. When this condition is evaluated,
+        the data contained at 'contents.mime_type' might be 'image/jpeg'.
+        In this case the evaluation would return True.
+
+        Args:
+            raw_query_string: A "query string" describing the target of the
+                condition. For example; "contents.mime_type".
+            raw_expression: A expression to use when evaluating this condition.
+        """
+        self._query_string = raw_query_string
+        self._expression = raw_expression
+
+    @property
+    def query_string(self):
+        return self._query_string
+
+    @query_string.setter
+    def query_string(self, raw_query_string):
+        valid_query_string = self._validate_query_string(raw_query_string)
+        if valid_query_string:
+            self._query_string = valid_query_string
+        else:
+            raise TypeError(
+                'Invalid query string: "{!s}"'.format(raw_query_string)
+            )
+
+    @property
+    def expression(self):
+        return self._expression
+
+    @expression.setter
+    def expression(self, raw_expression):
+        # TODO: [TD0001] Implement this properly!
+        # Workaround for 'metadata.exiftool.EXIF:DateTimeOriginal' ..
+        # Above test would return 'EXIF:DateTimeOriginal' but this solution
+        # would require testing the second to last part; 'exiftool', instead.
+        if self.query_string.startswith('metadata.exiftool'):
+            # TODO: [TD0015] Handle expression in 'condition_value'
+            #                ('Defined', '> 2017', etc)
+            if raw_expression:
+                self._expression = raw_expression
+                return
+
+        # The "query string" is required in order to know how the expression
+        # should be evaluated. Consider the expression invalid.
+        if not self.query_string:
+            return
+        parser = field_parsers.suitable_field_parser_for(self.query_string)
+        if parser.validate(raw_expression):
+            self._expression = raw_expression
+
+    @staticmethod
+    def _validate_query_string(raw_query_string):
+        if not raw_query_string:
+            return False
+
+        # TODO: [TD0001] Implement this properly!
+        # Workaround for 'metadata.exiftool.EXIF:DateTimeOriginal' ..
+        # Above test would return 'EXIF:DateTimeOriginal' but this solution
+        # would require testing the second to last part; 'exiftool', instead.
+        if raw_query_string.startswith('metadata.exiftool'):
+            # TODO: [TD0015] Handle expression in 'condition_value'
+            #                ('Defined', '> 2017', etc)
+            return True
+
+        # Get the last part of the field; 'mime_type' for 'contents.mime_type'.
+        field_components = util.query_string_list(raw_query_string)
+        field = field_components[-1:][0]
+
+        if field_parsers.suitable_field_parser_for(field):
+            return True
+        return False
---
+++ tests/unit_test_conditions.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from core.config.conditions import RuleCondition
+
+
+class TestRuleCondition(TestCase):
+    def setUp(self):
+        self.rc = RuleCondition(None, None)
+
+    def test_rulecondition_is_defined(self):
+        self.assertIsNotNone(self.rc)
+
+
+class TestRuleConditionFromValidInput(TestCase):
+    def test_contents_mime_type_condition(self):
+        self.rc = RuleCondition('contents.mime_type', 'text/rtf')
+        self.assertIsNotNone(self.rc)
+
+    def test_filesystem_basename_condition(self):
+        self.rc = RuleCondition('filesystem.basename', 'gmail.pdf')
+        self.assertIsNotNone(self.rc)
+
+    def test_filesystem_extension_condition(self):
+        self.rc = RuleCondition('filesystem.extension', 'pdf')
+        self.assertIsNotNone(self.rc)
+
+
+class TestRuleConditionFromInvalidInput(TestCase):
+    def test_invalid_contents_mime_type_condition(self):
+        with self.assertRaises(ValueError):
+            self.rc = RuleCondition('contents.mime_type', '/')
+
+    def test_invalid_filesystem_basename_condition(self):
+        with self.assertRaises(ValueError):
+            self.rc = RuleCondition('filesystem.basename', None)
+
+    def test_invalid_filesystem_extension_condition(self):
+        with self.assertRaises(ValueError):
+            self.rc = RuleCondition('filesystem.extension', None)
