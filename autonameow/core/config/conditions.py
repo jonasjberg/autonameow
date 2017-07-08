f92d7ad8c05367faa45d3a22bf922fd81c9690cd
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-08 18:43:53 +0200

    Implement '__str__' in the 'RuleCondition' class.

--- autonameow/core/config/conditions.py
+++ autonameow/core/config/conditions.py
@@ -136,3 +136,6 @@ class RuleCondition(object):
             return result
         else:
             return False
+
+    def __str__(self):
+        return '{!s}: {!s}'.format(self.query_string, self.expression)
