05fb4afc4c8b20df5a61d7e0e6ec7dc287511015
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-09 04:08:53 +0200

    Make function 'examine_rule' a method.
    Moves the function 'examine_rule' to a method '_examine_rule'.
    
    Makes sense as the function likely will not be called and state will
    have to be handled very differently in upcoming changes anyway.

--- autonameow/core/evaluate/rulematcher.py
+++ autonameow/core/evaluate/rulematcher.py
@@ -47,8 +47,7 @@ class RuleMatcher(object):
         # Check a copy of all rules.
         rules_to_examine = list(self.config.file_rules)
         log.debug('Examining {} rules ..'.format(len(rules_to_examine)))
-        ok_rules = self._examine_rules(rules_to_examine, self.file,
-                                       self.analysis_data)
+        ok_rules = self._examine_rules(rules_to_examine)
         if len(ok_rules) == 0:
             log.debug('No valid rules remain after evaluation')
             return
@@ -72,7 +71,7 @@ class RuleMatcher(object):
         for count, rule in enumerate(rules_to_examine):
             log.debug('Evaluating rule {}/{}: "{}"'.format(
                 count + 1, len(rules_to_examine), rule.description))
-            result = evaluate_rule(rule, self.file, self.analysis_data)
+            result = self._evaluate_rule(rule)
             if rule.exact_match and result is False:
                 log.debug('Rule evaluated FALSE, removing: '
                           '"{}"'.format(rule.description))
@@ -82,6 +81,52 @@ class RuleMatcher(object):
             ok_rules.append(rule)
 
         return ok_rules
+
+    def _evaluate_rule(self, file_rule):
+        """
+        Tests if a rule applies to a given file.
+
+        Returns at first unmatched condition if the rule requires an exact match.
+        If the rule does not require an exact match, all conditions are
+        evaluated and the rule is scored through "upvote()" and "downvote()".
+
+        Args:
+            file_rule: The rule to test as an instance of 'FileRule'.
+
+        Returns:
+            If the rule requires an exact match:
+                True if all rule conditions evaluates to True.
+                False if any rule condition evaluates to False.
+            If the rule does not require an exact match:
+                True
+        """
+        if not file_rule.conditions:
+            raise exceptions.InvalidFileRuleError(
+                'Rule does not specify any conditions'
+            )
+
+        if file_rule.exact_match:
+            for condition in file_rule.conditions:
+                log.debug('Evaluating condition "{!s}"'.format(condition))
+                if not eval_condition(condition, self.file, self.analysis_data):
+                    log.debug('Condition FAILED -- Exact match impossible ..')
+                    return False
+                else:
+                    file_rule.upvote()
+            return True
+
+        for condition in file_rule.conditions:
+            log.debug('Evaluating condition "{!s}"'.format(condition))
+            if eval_condition(condition, self.file, self.analysis_data):
+                log.debug('Condition Passed rule.votes++')
+                file_rule.upvote()
+            else:
+                # NOTE: file_rule.downvote()?
+                # log.debug('Condition FAILED rule.votes--')
+                log.debug('Condition FAILED')
+
+        # Rule was not completely discarded but could have failed all tests.
+        return True
 
     @property
     def best_match(self):
@@ -104,55 +149,6 @@ def prioritize_rules(rules):
     """
     return sorted(rules, reverse=True,
                   key=operator.attrgetter('score', 'weight'))
-
-
-def evaluate_rule(file_rule, file_object, analysis_data):
-    """
-    Tests if a rule applies to a given file.
-
-    Returns at first unmatched condition if the rule requires an exact match.
-    If the rule does not require an exact match, all conditions are
-    evaluated and the rule is scored through "upvote()" and "downvote()".
-
-    Args:
-        file_object: The file to test as an instance of 'FileObject'.
-        file_rule: The rule to test as an instance of 'FileRule'.
-        analysis_data: Results data from analysis of the given file.
-
-    Returns:
-        If the rule requires an exact match:
-            True if all rule conditions evaluates to True.
-            False if any rule condition evaluates to False.
-        If the rule does not require an exact match:
-            True
-    """
-    if not file_rule.conditions:
-        raise exceptions.InvalidFileRuleError(
-            'Rule does not specify any conditions'
-        )
-
-    if file_rule.exact_match:
-        for condition in file_rule.conditions:
-            log.debug('Evaluating condition "{!s}"'.format(condition))
-            if not eval_condition(condition, file_object, analysis_data):
-                log.debug('Condition FAILED -- Exact match impossible ..')
-                return False
-            else:
-                file_rule.upvote()
-        return True
-
-    for condition in file_rule.conditions:
-        log.debug('Evaluating condition "{!s}"'.format(condition))
-        if eval_condition(condition, file_object, analysis_data):
-            log.debug('Condition Passed rule.votes++')
-            file_rule.upvote()
-        else:
-            # NOTE: file_rule.downvote()?
-            # log.debug('Condition FAILED rule.votes--')
-            log.debug('Condition FAILED')
-
-    # Rule was not completely discarded but could still have failed all tests.
-    return True
 
 
 def eval_condition(condition, file_object, analysis_data):
