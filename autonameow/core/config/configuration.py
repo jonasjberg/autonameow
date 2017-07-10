d41e7f68ce5d89c637d36f9995dda7593c9ef580
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-10 20:25:43 +0200

    Remove completed TODO.

--- autonameow/core/config/configuration.py
+++ autonameow/core/config/configuration.py
@@ -83,9 +83,6 @@ class FileRule(Rule):
             out.append('{}="{}"'.format(key.title(), self.__dict__[key]))
         return 'FileRule({})'.format(', '.join(out))
 
-    # TODO: [TD0046] Make sure `FileRule` scores aren't shared between files.
-    # TODO: Should 'FileRule' objects be re-created for each file?
-    # TODO: Store score in a dictionary keyed by files?
     def upvote(self):
         """
         Increases the matching score of this rule.
