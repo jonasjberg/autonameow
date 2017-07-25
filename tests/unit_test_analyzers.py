eb9b9e86aabac9f56c85b4ccfb99e8dd5d94fd60
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-25 02:27:42 +0200

    Move 'BaseAnalyzer' unit tests to match moved code.

--- tests/unit_test_analyzers.py
+++ tests/unit_test_analyzers.py
@@ -22,8 +22,13 @@
 
 from unittest import TestCase
 
-import analyzers
 import unit_utils as uu
+import analyzers
+from core import (
+    exceptions,
+    constants
+)
+
 
 # TODO: [hardcoded] Likely to break; fixed analyzer names!
 EXPECT_ANALYZER_CLASSES = ['analyzers.analyze_image.ImageAnalyzer',
@@ -34,6 +39,70 @@ EXPECT_ANALYZER_CLASSES = ['analyzers.analyze_image.ImageAnalyzer',
                            'analyzers.analyze_text.TextAnalyzer']
 EXPECT_ANALYZER_CLASSES_BASENAME = [c.split('.')[-1]
                                     for c in EXPECT_ANALYZER_CLASSES]
+
+
+class TestBaseAnalyzer(TestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.a = analyzers.BaseAnalyzer(uu.get_mock_fileobject(), None, None)
+
+        class DummyFileObject(object):
+            def __init__(self):
+                self.mime_type = 'image/jpeg'
+        self.dummy_fo = DummyFileObject()
+
+    def test_run_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.run()
+
+    def test_get_with_invalid_field_name_raises_exception(self):
+        with self.assertRaises(exceptions.AnalysisResultsFieldError):
+            self.a.get('not_a_field')
+
+    def test_get_with_invalid_non_callable_field_name_raises_exception(self):
+        with self.assertRaises(exceptions.AnalysisResultsFieldError):
+            self.a.get('__str__')
+
+    def test_get_with_none_field_name_raises_exception(self):
+        with self.assertRaises(exceptions.AnalysisResultsFieldError):
+            self.a.get(None)
+
+    def test_get_with_valid_field_name_raises_not_implemented_error(self):
+        _field_name = constants.ANALYSIS_RESULTS_FIELDS[0]
+        with self.assertRaises(NotImplementedError):
+            self.a.get(_field_name)
+
+    def test_get_with_valid_field_names_raises_not_implemented_error(self):
+        for _field_name in constants.ANALYSIS_RESULTS_FIELDS:
+            with self.assertRaises(NotImplementedError):
+                self.a.get(_field_name)
+
+    def test_get_datetime_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.get_datetime()
+
+    def test_get_title_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.get_title()
+
+    def test_get_author_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.get_author()
+
+    def test_get_tags_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.get_tags()
+
+    def test_get_publisher_raises_not_implemented_error(self):
+        with self.assertRaises(NotImplementedError):
+            self.a.get_publisher()
+
+    def test_class_method_can_handle_is_defined_and_does_not_return_none(self):
+        self.assertIsNotNone(self.a.can_handle)
+        self.assertIsNotNone(self.a.can_handle(self.dummy_fo))
+
+    def test_class_method_can_handle_returns_false(self):
+        self.assertFalse(self.a.can_handle(self.dummy_fo))
 
 
 class TestFindAnalyzerSourceFiles(TestCase):
