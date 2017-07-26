241414d3dc5106a8a853170e89fff422bb1f1d07
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-27 01:10:33 +0200

    Start reworking the 'PdfAnalyzer'.
    Adds various fixes to the 'PdfAnalyzer' class.
    
    Remove broken functionality and unused fields. Modify methods to work
    with current "query string" method of accessing extracted data.

--- autonameow/analyzers/analyze_pdf.py
+++ autonameow/analyzers/analyze_pdf.py
@@ -37,83 +37,70 @@ class PdfAnalyzer(BaseAnalyzer):
         )
         self.add_results = add_results_callback
 
-        self.meta_extractor = None
-
-        self.exiftool = None
-        self.exiftool_data = None
-
-        self.metadata = None
         self.text = None
 
+    def _add_results(self, label, data):
+        query_string = 'analysis.pdfanalyzer.{}'.format(label)
+        logging.debug('{} passed "{}" to "add_results" callback'.format(
+            self, query_string)
+        )
+        self.add_results(query_string, data)
+
     def run(self):
-        self.metadata = self.extracted_data.get('metadata.exiftool')
         self.text = self.extracted_data.get('contents.textual.raw_text')
 
-    def __collect_results(self, query_string, src_dict, src_field, weight):
-        if not src_dict:
-            return []
+        # Pass results through callback function provided by the 'Analysis'.
+        self._add_results('author', self.get_author())
+        self._add_results('title', self.get_title())
+        self._add_results('datetime', self.get_datetime())
+        self._add_results('publisher', self.get_publisher())
 
-        if src_field in src_dict:
-            value = src_dict.get(src_field)
+    def __collect_results(self, query_string, weight):
+        value = self.extracted_data.get(query_string)
+        if value:
             return result_list_add(value, query_string, weight)
         else:
             return []
 
     def get_author(self):
-        # TODO: [TD0005] Remove, use callbacks instead.
         results = []
 
         possible_authors = [
-            ('metadata.exiftool.PDF:Author', self.exiftool_data,
-             'PDF:Author', 1),
-            ('metadata.exiftool.PDF:Creator', self.exiftool_data,
-             'PDF:Creator', 0.8),
-            ('metadata.exiftool.PDF:Producer', self.exiftool_data,
-             'PDF:Producer', 0.8),
-            ('metadata.exiftool.XMP:Creator', self.exiftool_data,
-             'XMP:Creator', 0.8),
-            ('metadata.pypdf.Author', self.metadata, 'Author', 1),
-            ('metadata.pypdf.Creator', self.metadata, 'Creator', 0.8),
-            ('metadata.pypdf.Producer', self.metadata, 'Producer', 0.5)]
+            ('metadata.exiftool.PDF:Author', 1),
+            ('metadata.exiftool.PDF:Creator', 0.8),
+            ('metadata.exiftool.PDF:Producer', 0.8),
+            ('metadata.exiftool.XMP:Creator', 0.8),
+            ('metadata.pypdf.Author', 1),
+            ('metadata.pypdf.Creator',  0.8),
+            ('metadata.pypdf.Producer',  0.5)
+        ]
+        for query_string, weight, in possible_authors:
+            results += self.__collect_results(query_string, weight)
 
-        for query_string, source_dict, source_field, weight in possible_authors:
-            results += self.__collect_results(query_string, source_dict,
-                                              source_field, weight)
         return results
 
     def get_title(self):
-        # TODO: [TD0005] Remove, use callbacks instead.
         results = []
 
         possible_titles = [
-            ('metadata.exiftool.PDF:Title', self.exiftool_data,
-             'PDF:Title', 1),
-            ('metadata.exiftool.XMP:Title', self.exiftool_data,
-             'XMP:Title', 8),
-            ('metadata.exiftool.PDF:Subject', self.exiftool_data,
-             'PDF:Subject', 0.25),
-            ('metadata.pypdf.Title', self.metadata, 'Title', 1),
-            ('metadata.pypdf.Subject', self.metadata, 'Creator', 0.25)]
-
-        for query_string, source_dict, source_field, weight in possible_titles:
-            results += self.__collect_results(query_string, source_dict,
-                                              source_field, weight)
+            ('metadata.exiftool.PDF:Title', 1),
+            ('metadata.exiftool.XMP:Title', 8),
+            ('metadata.exiftool.PDF:Subject', 0.25),
+            ('metadata.pypdf.Title', 1),
+            ('metadata.pypdf.Subject', 0.25)
+        ]
+        for query_string, weight in possible_titles:
+            results += self.__collect_results(query_string, weight)
 
         return results
 
     def get_datetime(self):
-        # TODO: [TD0005] Remove, use callbacks instead.
         results = []
 
-        if self.metadata:
-            metadata_datetime = self._get_metadata_datetime()
-            if metadata_datetime:
-                results += metadata_datetime
-
-            if self.text:
-                text_timestamps = self._get_datetime_from_text()
-                if text_timestamps:
-                    results += text_timestamps
+        if self.text:
+            text_timestamps = self._get_datetime_from_text()
+            if text_timestamps:
+                results += text_timestamps
 
         return results
 
@@ -122,99 +109,15 @@ class PdfAnalyzer(BaseAnalyzer):
         raise NotImplementedError('Get "tags" from PdfAnalyzer')
 
     def get_publisher(self):
-        # TODO: [TD0005] Remove, use callbacks instead.
         results = []
 
         possible_publishers = [
-            ('metadata.exiftool.PDF:EBX_PUBLISHER', self.exiftool_data,
-             'PDF:EBX_PUBLISHER', 1),
-            ('metadata.exiftool.XMP:EbxPublisher', self.exiftool_data,
-             'XMP:EbxPublisher', 1),
-            ('metadata.pypdf.EBX_PUBLISHER', self.metadata,
-             'EBX_PUBLISHER', 1)]
-
-        for query_string, src_dict, src_field, weight in possible_publishers:
-            results += self.__collect_results(query_string, src_dict,
-                                              src_field, weight)
-        return results
-
-    def _get_metadata_datetime(self):
-        """
-        Extract date and time information from pdf metadata.
-        :return: dict of datetime-objects
-        """
-
-        # TODO: [TD0006] This method is moved to a function in 'metadata.py' (?)
-
-        DATE_TAG_FIELDS = ['ModDate', 'CreationDate', 'ModDate']
-
-        results = []
-        for field in DATE_TAG_FIELDS:
-            date = time = k = None
-            if field in self.metadata:
-                try:
-                    k = self.metadata[field]
-                    k = k.strip()
-                except KeyError:
-                    logging.error('KeyError for key [{}]'.format(field))
-                    continue
-
-            if k is None:
-                logging.debug('Null value in metadata field [%s]' % field)
-                continue
-
-            found_match = False
-            dt = None
-            # Expected date format:             D:20121225235237+05'30'
-            #
-            # Regex search matches two groups:  D: 20121225235237 +05'30'
-            #                                      ^            ^ ^     ^
-            #                                      '------------' '-----'
-            #                                            #1         #2
-            #
-            date_pattern_with_tz = re.compile('D:(\d{14})(\+\d{2}\'\d{2}\')')
-            re_match_tz = date_pattern_with_tz.search(k)
-            if re_match_tz:
-                datetime_str = re_match_tz.group(1)
-                timezone_str = re_match_tz.group(2)
-                timezone_str = timezone_str.replace("'", "")
-                logging.debug('datetime_str: %s' % datetime_str)
-                logging.debug('timezone_str: %s' % timezone_str)
-
-                try:
-                    dt = datetime.strptime(str(datetime_str + timezone_str),
-                                           "%Y%m%d%H%M%S%z")
-                    found_match = True
-                except ValueError:
-                    logging.debug('Unable to parse aware datetime from '
-                                  '[%s]' % field)
-
-                if not found_match:
-                    try:
-                        dt = datetime.strptime(datetime_str, "%Y%m%d%H%M%S")
-                        found_match = True
-                    except ValueError:
-                        logging.debug('Unable to parse naive datetime from '
-                                      '[%s]' % field)
-
-            # Try matching another pattern.
-            date_pattern_no_tz = re.compile('D:(\d{14})Z')
-            re_match = date_pattern_no_tz.search(k)
-            if re_match:
-                try:
-                    dt = datetime.strptime(re_match.group(1), '%Y%m%d%H%M%S')
-                    found_match = True
-                except ValueError:
-                    logging.debug('Unable to parse datetime from '
-                                  'metadata field [%s]' % field)
-                    continue
-
-            if found_match:
-                results.append({'value': dt,
-                                'source': field,
-                                'weight': 1})
-                logging.debug('Extracted date/time from pdf metadata field '
-                              '"{}": "{}"'.format(field, dt))
+            ('metadata.exiftool.PDF:EBX_PUBLISHER', 1),
+            ('metadata.exiftool.XMP:EbxPublisher', 1),
+            ('metadata.pypdf.EBX_PUBLISHER', 1)
+        ]
+        for query_string, weight in possible_publishers:
+            results += self.__collect_results(query_string, weight)
 
         return results
 
