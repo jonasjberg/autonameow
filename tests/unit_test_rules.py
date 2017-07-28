c87ba2d4219301caa6db119e968c83c0bcb7f044
Jonas Sjöberg <jomeganas@gmail.com>
2017-07-28 14:53:42 +0200

    Add unit tests for 'get_valid_rule_condition'.

--- tests/unit_test_rules.py
+++ tests/unit_test_rules.py
@@ -112,3 +112,48 @@ class TestFileRuleMethods(TestCase):
     def test_filerule_string(self):
         actual = str(self.filerule)
         self.assertTrue(isinstance(actual, str))
+
+
+class TestGetValidRuleCondition(TestCase):
+    def test_get_valid_rule_condition_is_defined(self):
+        self.assertIsNotNone(rules.get_valid_rule_condition)
+
+    def _assert_valid(self, query, data):
+        actual = rules.get_valid_rule_condition(query, data)
+        self.assertIsNotNone(actual)
+        self.assertTrue(isinstance(actual, rules.RuleCondition))
+
+    def _assert_invalid(self, query, data):
+        actual = rules.get_valid_rule_condition(query, data)
+        self.assertFalse(actual)
+
+    def test_returns_valid_rule_condition_for_valid_query_valid_data(self):
+        self._assert_valid('contents.mime_type', 'application/pdf')
+        self._assert_valid('contents.mime_type', 'text/rtf')
+        self._assert_valid('contents.mime_type', 'image/*')
+        self._assert_valid('filesystem.basename.extension', 'pdf')
+        self._assert_valid('filesystem.basename.full', 'foo.pdf')
+        self._assert_valid('filesystem.pathname.full', '~/temp/foo')
+
+    def test_returns_false_for_invalid_query_valid_data(self):
+        self._assert_invalid('', 'application/pdf')
+        self._assert_invalid('c.m', 'text/rtf')
+        self._assert_invalid(None, 'pdf')
+        self._assert_invalid('None', 'pdf')
+        self._assert_invalid('filesystem..full', 'foo.pdf')
+        self._assert_invalid('foo', '~/temp/foo')
+
+    def test_returns_false_for_valid_query_invalid_data(self):
+        self._assert_invalid('contents.mime_type', 'application/*//pdf')
+        self._assert_invalid('contents.mime_type', 'application///pdf')
+        self._assert_invalid('contents.mime_type', 'text/')
+        self._assert_invalid('filesystem.basename.extension', '')
+        self._assert_invalid('filesystem.basename.full', None)
+        self._assert_invalid('filesystem.pathname.full', '')
+
+    def test_returns_false_for_invalid_query_invalid_data(self):
+        self._assert_invalid('', 'application///pdf')
+        self._assert_invalid('c.m', '')
+        self._assert_invalid('foo', None)
+        self._assert_invalid(None, 'foo')
+        self._assert_invalid(None, None)
