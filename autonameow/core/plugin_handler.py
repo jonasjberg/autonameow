9946d06a6af6dc1480da230a5b2778d5a092f438
Jonas Sjöberg <jomeganas@gmail.com>
2017-08-01 00:55:27 +0200

    Add new file 'plugin_handler.py'.
    Adds a new file 'plugin_handler.py' with class 'PluginResults'.

---
+++ autonameow/core/plugin_handler.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+import plugins
+from core.container import DataContainerBase
+
+
+class PluginResults(DataContainerBase):
+    def __init__(self):
+        super(PluginResults, self).__init__()
+
+    def get(self, query_string=None):
+        if not query_string:
+            return self._data
+
+        if query_string.startswith('plugin.'):
+            plugin_name, plugin_query = query_string.lstrip('plugin.').split('.')
+            result = plugins.plugin_query(plugin_name, plugin_query, None)
+            return result
+        else:
+            return False
+
+    def add(self, destination, data):
+        if not destination:
+            raise KeyError('Missing results field')
+
+        self._data.update({field: data})
