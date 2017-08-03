7642ba3131c035a94e95a54b15e70fd67be8a72c
Jonas Sjöberg <jomeganas@gmail.com>
2017-08-03 22:38:15 +0200

    Move 'PyPDFMetadataExtractor' to 'metadata_pypdf.py'.
    Moves 'PyPDFMetadataExtractor' to new file 'metadata_pypdf.py'.

--- autonameow/extractors/metadata.py
+++ autonameow/extractors/metadata.py
@@ -21,13 +21,7 @@
 
 import logging as log
 
-import PyPDF2
-from PyPDF2.utils import (
-    PdfReadError
-)
-
 from core import (
-    util,
     types
 )
 from core.exceptions import ExtractorError
@@ -107,70 +101,3 @@ class AbstractMetadataExtractor(BaseExtractor):
 
     def _get_raw_metadata(self):
         raise NotImplementedError('Must be implemented by inheriting classes.')
-
-
-class PyPDFMetadataExtractor(AbstractMetadataExtractor):
-    handles_mime_types = ['application/pdf']
-    data_query_string = 'metadata.pypdf'
-
-    tagname_type_lookup = {
-        'Creator': types.AW_STRING,
-        'CreationDate': types.AW_PYPDFTIMEDATE,
-        'Encrypted': types.AW_BOOLEAN,
-        'ModDate': types.AW_PYPDFTIMEDATE,
-        'NumberPages': types.AW_INTEGER,
-        'Paginated': types.AW_BOOLEAN,
-        'Producer': types.AW_STRING,
-    }
-
-    def __init__(self, source):
-        super(PyPDFMetadataExtractor, self).__init__(source)
-        self._raw_metadata = None
-
-    def _get_raw_metadata(self):
-        try:
-            return self._get_pypdf_data()
-        except Exception as e:
-            raise ExtractorError(e)
-
-    def _get_pypdf_data(self):
-        out = {}
-
-        try:
-            # NOTE(jonas): [encoding] Double-check PyPDF2 docs ..
-            file_reader = PyPDF2.PdfFileReader(util.decode_(self.source), 'rb')
-        except Exception:
-            # TODO: Raise custom exception .. ?
-            raise
-        else:
-            doc_info = file_reader.getDocumentInfo()
-            if doc_info:
-                # Remove any leading '/' from all dict keys.
-                out = {k.lstrip('\/'): v for k, v in doc_info.items()}
-
-                # Convert PyPDF values of type 'PyPDF2.generic.TextStringObject'
-                out = {k: str(v) for k, v in out.items()}
-
-            out.update({'Encrypted': file_reader.isEncrypted})
-
-            try:
-                num_pages = file_reader.getNumPages()
-            except PdfReadError:
-                # PDF document might be encrypted with restrictions for reading.
-                # TODO: Raise custom exception .. ?
-                raise
-            else:
-                out.update({'NumberPages': num_pages})
-                out.update({'Paginated': True})
-
-            # https://pythonhosted.org/PyPDF2/XmpInformation.html
-            xmp_metadata = file_reader.getXmpMetadata()
-            if xmp_metadata:
-                xmp = {}
-                for k, v in xmp_metadata.items():
-                    if v:
-                        xmp[k] = v
-
-                out.update(xmp)
-
-        return out
---
+++ autonameow/extractors/metadata_pypdf.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+import PyPDF2
+from PyPDF2.utils import PdfReadError
+
+from core import (
+    types,
+    util
+)
+from core.exceptions import ExtractorError
+from extractors.metadata import AbstractMetadataExtractor
+
+
+class PyPDFMetadataExtractor(AbstractMetadataExtractor):
+    handles_mime_types = ['application/pdf']
+    data_query_string = 'metadata.pypdf'
+
+    tagname_type_lookup = {
+        'Creator': types.AW_STRING,
+        'CreationDate': types.AW_PYPDFTIMEDATE,
+        'Encrypted': types.AW_BOOLEAN,
+        'ModDate': types.AW_PYPDFTIMEDATE,
+        'NumberPages': types.AW_INTEGER,
+        'Paginated': types.AW_BOOLEAN,
+        'Producer': types.AW_STRING,
+    }
+
+    def __init__(self, source):
+        super(PyPDFMetadataExtractor, self).__init__(source)
+        self._raw_metadata = None
+
+    def _get_raw_metadata(self):
+        try:
+            return self._get_pypdf_data()
+        except Exception as e:
+            raise ExtractorError(e)
+
+    def _get_pypdf_data(self):
+        out = {}
+
+        try:
+            # NOTE(jonas): [encoding] Double-check PyPDF2 docs ..
+            file_reader = PyPDF2.PdfFileReader(util.decode_(self.source), 'rb')
+        except Exception:
+            # TODO: Raise custom exception .. ?
+            raise
+        else:
+            doc_info = file_reader.getDocumentInfo()
+            if doc_info:
+                # Remove any leading '/' from all dict keys.
+                out = {k.lstrip('\/'): v for k, v in doc_info.items()}
+
+                # Convert PyPDF values of type 'PyPDF2.generic.TextStringObject'
+                out = {k: str(v) for k, v in out.items()}
+
+            out.update({'Encrypted': file_reader.isEncrypted})
+
+            try:
+                num_pages = file_reader.getNumPages()
+            except PdfReadError:
+                # PDF document might be encrypted with restrictions for reading.
+                # TODO: Raise custom exception .. ?
+                raise
+            else:
+                out.update({'NumberPages': num_pages})
+                out.update({'Paginated': True})
+
+            # https://pythonhosted.org/PyPDF2/XmpInformation.html
+            xmp_metadata = file_reader.getXmpMetadata()
+            if xmp_metadata:
+                xmp = {}
+                for k, v in xmp_metadata.items():
+                    if v:
+                        xmp[k] = v
+
+                out.update(xmp)
+
+        return out
