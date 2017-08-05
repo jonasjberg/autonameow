jonasjberg/autonameow

commit 18866c235f64f25f88585ee23c66d65a29a0e5d8

Add assertion for number of suitable parsers.
Adds asserting that ONE parser should be "mapped" to a query string.

autonameow/core/config/rules.py
@@ -132,6 +132,8 @@ class RuleCondition(object):
 
         parsers = field_parsers.suitable_field_parser_for(query_string)
         if parsers:
+            # Assume only one parser can handle a query string for now.
+            assert(len(parsers) == 0)
             self._parser = parsers[0]
             return self._parser
         else:

commit 5c41175277f57781c8b32961c05bb827078c815d

Fix bad assertion.
Fixes bad assertion introduced in 18866c235f64f25f88585ee23c66d65a.

autonameow/core/config/rules.py
@@ -133,7 +133,7 @@ class RuleCondition(object):
         parsers = field_parsers.suitable_field_parser_for(query_string)
         if parsers:
             # Assume only one parser can handle a query string for now.
-            assert(len(parsers) == 0)
+            assert(len(parsers) == 1)
             self._parser = parsers[0]
             return self._parser
         else: