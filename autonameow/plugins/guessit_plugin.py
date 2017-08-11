c62b2539b6d7063c8c3ba59c88fbc5c7e8680d24
Jonas Sjöberg <jomeganas@gmail.com>
2017-08-11 18:49:19 +0200

    Rename 'guessit_metadata' to 'guessit_results'.

--- autonameow/plugins/guessit_plugin.py
+++ autonameow/plugins/guessit_plugin.py
@@ -31,32 +31,32 @@ class GuessitPlugin(BasePlugin):
     def __init__(self):
         super(GuessitPlugin, self).__init__(display_name='Guessit')
 
-        self.guessit_metadata = {}
+        self.guessit_results = {}
 
     def test_init(self):
         return guessit is not False
 
     def query(self, field=None):
-        if not self.guessit_metadata:
+        if not self.guessit_results:
             # TODO: Pass input data (basename of current file) to guessit ..
-            # self.guessit_metadata = run_guessit(file_object.filenamepart_base)
+            # self.guessit_results = run_guessit(file_object.filenamepart_base)
             pass
 
     # def _get_datetime(self):
     #     result = []
-    #     if self.guessit_metadata:
-    #         guessit_timestamps = self._get_datetime_from_guessit_metadata()
+    #     if self.guessit_results:
+    #         guessit_timestamps = self._get_datetime_from_guessit_results()
     #         if guessit_timestamps:
     #             result += guessit_timestamps
     #
     # def _run(self):
     #     if guessit and self.file_object.mime_type == 'mp4':
-    #         self.guessit_metadata = self._get_metadata_from_guessit()
+    #         self.guessit_results = self._get_metadata_from_guessit()
     #
-    #         if self.guessit_metadata:
-    #             self.add_results('plugins.guessit', self.guessit_metadata)
+    #         if self.guessit_results:
+    #             self.add_results('plugins.guessit', self.guessit_results)
     #
-    # def _get_datetime_from_guessit_metadata(self):
+    # def _get_datetime_from_guessit_results(self):
     #     """
     #     Get date/time-information from the results returned by "guessit".
     #     :return: a list of dictionaries (actually just one) on the form:
@@ -65,13 +65,13 @@ class GuessitPlugin(BasePlugin):
     #                  'weight'  : 1
     #                }, .. ]
     #     """
-    #     if self.guessit_metadata:
-    #         if 'date' in self.guessit_metadata:
-    #             return [{'value': self.guessit_metadata['date'],
+    #     if self.guessit_results:
+    #         if 'date' in self.guessit_results:
+    #             return [{'value': self.guessit_results['date'],
     #                      'source': 'guessit',
     #                      'weight': 0.75}]
     #
-    # def _get_title_from_guessit_metadata(self):
+    # def _get_title_from_guessit_results(self):
     #     """
     #     Get the title from the results returned by "guessit".
     #     :return: a list of dictionaries (actually just one) on the form:
@@ -80,9 +80,9 @@ class GuessitPlugin(BasePlugin):
     #                  'weight'  : 0.75
     #                }, .. ]
     #     """
-    #     if self.guessit_metadata:
-    #         if 'title' in self.guessit_metadata:
-    #             return [{'value': self.guessit_metadata['title'],
+    #     if self.guessit_results:
+    #         if 'title' in self.guessit_results:
+    #             return [{'value': self.guessit_results['title'],
     #                      'source': 'guessit',
     #                      'weight': 0.75}]
 
