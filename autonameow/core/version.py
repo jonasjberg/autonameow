202d33d4e08b7b1b8bd517adbabfd336d89c7ad2
Jonas Sjöberg <jomeganas@gmail.com>
2017-08-26 16:18:28 +0200

    Add integer tuple version number in 'version.py'.
    Adds int tuple '__version_info__' to hold the version number.
    
    Modifies '__version__' to convert '__version_info__' to a string.

--- autonameow/core/version.py
+++ autonameow/core/version.py
@@ -27,7 +27,8 @@
 # Source: http://stackoverflow.com/a/16084844
 
 __title__ = 'autonameow'
-__version__ = '0.4.5'
+__version_info__ = (0, 4, 5)
+__version__ = '.'.join(map(str, __version_info__))
 __author__ = 'Jonas Sjöberg'
 __email__ = 'jomeganas[at]gmail.com'
 __url__ = 'www.jonasjberg.com'
