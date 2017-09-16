jonasjberg/autonameow

commit eee912963f716886001b1f63ba445f9eef72f8e0

Move "flattening" data out of 'Repository.store()'.
Removes "flattening" of in the 'Repository' before being stored.

Moves this to the local 'collect_results' functions that is passed
to extractors and analyzers.

This is motivated by attempting to minimize uncertainty of which
types of data structures the 'Repository' receives.
The "non-core" components have been changed so much, with so many
hack fixes and partial experiment added on, so that a common
interface barely exists any longer. This must be improved.

autonameow/core/analysis.py
@@ -100,18 +100,34 @@ def request_global_data(file_object, meowuri):
         return response
 
 
-def collect_results(file_object, label, data):
+def collect_results(file_object, meowuri, data):
     """
     Collects analysis results. Passed to analyzers as a callback.
 
     Analyzers call this to store results data in the session repository.
 
+    If argument "data" is a dictionary, it is "flattened" here.
+    Example:
+
+      Incoming arguments:
+        MeowURI: 'metadata.exiftool'     DATA: {'a': 'b', 'c': 'd'}
+
+      Would be "flattened" to:
+        MeowURI: 'metadata.exiftool.a'   DATA: 'b'
+        MeowURI: 'metadata.exiftool.c'   DATA: 'd'
+
     Args:
         file_object: Instance of 'file_object' that produced the data to add.
-        label: Label that uniquely identifies the data, as a Unicode str.
+        meowuri: Label that uniquely identifies the data, as a Unicode str.
         data: The data to add, as any type or container.
     """
-    repository.SessionRepository.store(file_object, label, data)
+    if isinstance(data, dict):
+        flat_data = util.flatten_dict(data)
+        for _key, _data in flat_data.items():
+            _uri = '{}.{!s}'.format(meowuri, _key)
+            repository.SessionRepository.store(file_object, _uri, _data)
+
+    repository.SessionRepository.store(file_object, meowuri, data)
 
 
 def _instantiate_analyzers(file_object, klass_list):

autonameow/core/extraction.py
@@ -23,7 +23,8 @@ import logging
 
 import extractors
 from core import (
-    repository
+    repository,
+    util
 )
 from extractors import ExtractorError
 
@@ -31,16 +32,32 @@ from extractors import ExtractorError
 log = logging.getLogger(__name__)
 
 
-def collect_results(file_object, label, data):
+def collect_results(file_object, meowuri, data):
     """
     Collects extractor data, passes it the the session repository.
 
+    If argument "data" is a dictionary, it is "flattened" here.
+    Example:
+
+      Incoming arguments:
+        MeowURI: 'metadata.exiftool'     DATA: {'a': 'b', 'c': 'd'}
+
+      Would be "flattened" to:
+        MeowURI: 'metadata.exiftool.a'   DATA: 'b'
+        MeowURI: 'metadata.exiftool.c'   DATA: 'd'
+
     Args:
         file_object: File that produced the data to add.
-        label: Label that uniquely identifies the data.
+        meowuri: Label that uniquely identifies the data.
         data: The data to add.
     """
-    repository.SessionRepository.store(file_object, label, data)
+    if isinstance(data, dict):
+        flat_data = util.flatten_dict(data)
+        for _key, _data in flat_data.items():
+            _uri = '{}.{!s}'.format(meowuri, _key)
+            repository.SessionRepository.store(file_object, _uri, _data)
+
+    repository.SessionRepository.store(file_object, meowuri, data)
 
 
 def keep_slow_extractors_if_required(extractor_klasses, required_extractors):

autonameow/core/repository.py
@@ -72,25 +72,15 @@ class Repository(object):
 
             STORAGE = {
                 'file_object_A': {
-                    'meowuri_a': [1, 2]
-                    'meowuri_b': ['foo']
+                    'meowuri_a': 1
+                    'meowuri_b': 'foo'
+                    'meowuri_c': ExtractedData(...)
                 }
                 'file_object_B': {
                     'meowuri_a': ['bar']
                     'meowuri_b': [2, 1]
                 }
             }
-
-        If argument "data" is a dictionary, it is "flattened" here.
-        Example:
-
-          Incoming arguments:
-          LABEL: 'metadata.exiftool'     DATA: {'a': 'b', 'c': 'd'}
-
-          Would be "flattened" to:
-          LABEL: 'metadata.exiftool.a'   DATA: 'b'
-          LABEL: 'metadata.exiftool.c'   DATA: 'd'
-
         """
         if not meowuri:
             raise exceptions.InvalidDataSourceError(
@@ -106,17 +96,8 @@ class Repository(object):
                         ' "{!s}"'.format(meowuri))
             return
 
-        if isinstance(data, dict):
-            flat_data = util.flatten_dict(data)
-            for k, v in flat_data.items():
-                merged_meowuri = meowuri + '.' + str(k)
-                self._store(file_object, merged_meowuri, v)
-                self._store_generic(file_object, v)
-        else:
-            print('Repository got UNEXPECTED data: {{"{!s}": '
-                  '{{"{!s}": {!s}}}}}'.format(file_object, meowuri, data))
-            self._store(file_object, meowuri, data)
-            self._store_generic(file_object, data)
+        self._store(file_object, meowuri, data)
+        self._store_generic(file_object, data)
 
     def _store_generic(self, file_object, data):
         if not isinstance(data, extractors.ExtractedData):

commit 016156817b87cb21f31cd9fcdc735dbe06631423

Fix logic bug causing duplicated storage.
Fixes bug introduced in commit eee912963f716886001b1f63ba445f9eef.

autonameow/core/analysis.py
@@ -126,8 +126,8 @@ def collect_results(file_object, meowuri, data):
         for _key, _data in flat_data.items():
             _uri = '{}.{!s}'.format(meowuri, _key)
             repository.SessionRepository.store(file_object, _uri, _data)
-
-    repository.SessionRepository.store(file_object, meowuri, data)
+    else:
+        repository.SessionRepository.store(file_object, meowuri, data)
 
 
 def _instantiate_analyzers(file_object, klass_list):

autonameow/core/extraction.py
@@ -56,8 +56,8 @@ def collect_results(file_object, meowuri, data):
         for _key, _data in flat_data.items():
             _uri = '{}.{!s}'.format(meowuri, _key)
             repository.SessionRepository.store(file_object, _uri, _data)
-
-    repository.SessionRepository.store(file_object, meowuri, data)
+    else:
+        repository.SessionRepository.store(file_object, meowuri, data)
 
 
 def keep_slow_extractors_if_required(extractor_klasses, required_extractors):