54d53043748a7f664dfe0a29c90ffc547a48e3b3
Jonas Sjöberg <jomeganas@gmail.com>
2017-09-20 16:33:51 +0200

    Improve robustness + fixes in 'next_todo_id.py'.
    Adds checking that files exists along with other trivial fixes.

--- devscripts/next_todo_id.py
+++ devscripts/next_todo_id.py
@@ -22,23 +22,29 @@
 
 import os
 import re
+import sys
 
 '''
 Utility script for getting the next free (unused) TODO-list entry identifier.
 '''
 
-_THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-AUTONAMEOW_SRC_ROOT = os.path.normpath(os.path.join(_THIS_DIR, os.pardir))
-
 TODO_BASENAME = 'TODO.md'
 DONE_BASENAME = 'done.md'
-RE_TODO_ID = re.compile(r'\[TD(\d{4})\]')
+TODO_IDENTIFIER_FORMAT = '[TD{:04d}]'
+RE_TODO_IDENTIFIER = re.compile(r'\[TD(\d{4})\]')
+
+_THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+AUTONAMEOW_SRC_ROOT = os.path.normpath(os.path.join(_THIS_DIR, os.pardir))
 
 
 def find_todo_ids_in_file(file_path):
     for line in open(file_path, 'r', encoding='utf8'):
-        for match in re.finditer(RE_TODO_ID, line):
+        for match in re.finditer(RE_TODO_IDENTIFIER, line):
             found_ids.add(match.group(1))
+
+
+def is_readable_file(file_path):
+    return os.path.isfile(file_path) and os.access(file_path, os.R_OK)
 
 
 found_ids = set()
@@ -46,11 +52,16 @@ found_ids = set()
 todo_path = os.path.join(AUTONAMEOW_SRC_ROOT, TODO_BASENAME)
 done_path = os.path.join(AUTONAMEOW_SRC_ROOT, DONE_BASENAME)
 
+for _path in (todo_path, done_path):
+    if not is_readable_file(_path):
+        sys.exit('File does not exist or is not readable: "{!s}"'.format(_path))
+
+
 find_todo_ids_in_file(todo_path)
 find_todo_ids_in_file(done_path)
 
 if found_ids:
     last_id = sorted(found_ids, reverse=True)[0]
-    next_unused_id = '[TD{:04d}]'.format(int(last_id) + 1)
+    next_unused_id = TODO_IDENTIFIER_FORMAT.format(int(last_id) + 1)
 
     print(next_unused_id)
