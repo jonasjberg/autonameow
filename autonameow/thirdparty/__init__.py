f9827ea81754a82453323710fc6e069379b3aeb7
Jonas Sjöberg <jomeganas@gmail.com>
2017-09-24 18:30:17 +0200

    Import 'nameparser' from 'thirdparty/__init__.py'.

--- autonameow/thirdparty/__init__.py
+++ autonameow/thirdparty/__init__.py
@@ -19,6 +19,20 @@
 #   You should have received a copy of the GNU General Public License
 #   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
+
+"""
+Interface to third-party code stored under the 'thirdparty' directory.
+
+Auxiliary code included as Git submodules might be missing or the code might
+be broken in some other way. Always import third-party modules from this file.
+"""
+
+
+THIRDPARTY_ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+
 try:
     from .epubzilla.epubzilla import epubzilla
 except ImportError:
@@ -28,3 +42,17 @@ try:
     from .pyexiftool import exiftool as pyexiftool
 except ImportError:
     pyexiftool = None
+
+
+MODULE_NAMEPARSER_DIR = 'nameparser'
+_nameparser_path = os.path.join(THIRDPARTY_ROOT_DIR, MODULE_NAMEPARSER_DIR)
+
+if os.path.isdir(_nameparser_path):
+    sys.path.insert(0, _nameparser_path)
+
+    try:
+        import nameparser
+    except ImportError:
+        nameparser = None
+else:
+    nameparser = None
