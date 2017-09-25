jonasjberg/autonameow

commit ca4779cdb7e6235911c799cf3081fa80669b85c6

Use one "SessionRepository" per autonameow instance.
Adds a 'RepositoryPool' class to handle repositories.

The active repository is registered to the start time and
process ID registered by the 'Autonameow' class.
This solution is pretty overkill but does solve the problem.

Completes TODO-list item [TD0086].

autonameow/core/repository.py
@@ -416,6 +416,55 @@ def get_sources_for_meowuris(meowuri_list, includes=None):
     return list(out)
 
 
-# TODO: [TD0086] Keep one global 'SessionRepository' per 'Autonameow' instance.
-SessionRepository = Repository()
-SessionRepository.initialize()
+def _create_repository():
+    repository = Repository()
+    repository.initialize()
+    return repository
+
+
+class RepositoryPool(object):
+    DEFAULT_SESSION_ID = 'DEFAULT_SESSION'
+
+    def __init__(self):
+        self._repositories = {}
+
+    def get(self, id_=None):
+        if id_ is None:
+            id_ = self.DEFAULT_SESSION_ID
+
+        if id_ not in self._repositories:
+            raise KeyError('{} does not contain ID "{!s}'.format(self, id_))
+
+        return self._repositories.get(id_)
+
+    def add(self, repository=None, id_=None):
+        if id_ is None:
+            id_ = self.DEFAULT_SESSION_ID
+
+        if id_ in self._repositories:
+            raise KeyError('{} already contains ID "{!s}'.format(self, id_))
+
+        if repository is None:
+            _repo = _create_repository()
+        else:
+            _repo = repository
+
+        self._repositories[id_] = _repo
+
+    def _get_default(self):
+        if len(self._repositories) == 0:
+            pass
+
+
+def initialize(id_):
+    # Keep one global 'SessionRepository' per 'Autonameow' instance.
+    global Pool
+    Pool = RepositoryPool()
+    Pool.add(id_=id_)
+
+    global SessionRepository
+    SessionRepository = Pool.get(id_=id_)
+
+
+Pool = None
+SessionRepository = None

autonameow/core/main.py
@@ -93,6 +93,9 @@ class Autonameow(object):
             cli.print_ascii_banner()
             self.exit_program(C.EXIT_SUCCESS)
 
+        # Set up a session repository for this process.
+        repository.initialize(self)
+
         # Check configuration file. If no alternate config file path is
         # provided and no config file is found at default paths; copy the
         # template config and tell the user.
@@ -414,6 +417,9 @@ class Autonameow(object):
             log.debug('Exit code updated: {} -> {}'.format(self._exit_code,
                                                            value))
             self._exit_code = value
+
+    def __hash__(self):
+        return hash(util.process_id()) + hash(self.start_time)
 
 
 def _run_extraction(file_object, require_extractors, run_all_extractors=False):

autonameow/core/util/misc.py
@@ -26,6 +26,7 @@ Miscellaneous utility functions.
 import collections
 import itertools
 import logging
+import os
 import shutil
 import yaml
 
@@ -470,3 +471,7 @@ def filter_none(iterable):
     Removes any None values from the given iterable and returns the result.
     """
     return [item for item in iterable if item is not None]
+
+
+def process_id():
+    return os.getpid()

tests/unit_test_analysis.py
@@ -23,12 +23,16 @@ from unittest import TestCase
 import unit_utils as uu
 
 import analyzers
-from core import analysis
+from core import (
+    analysis,
+    repository
+)
 
 
 class TestAnalysis(TestCase):
     def setUp(self):
         self.fo = uu.get_mock_fileobject()
+        repository.initialize('foo')
 
     def test_analysis_start_requires_file_object_argument(self):
         with self.assertRaises(TypeError):

tests/unit_test_rulematcher.py
@@ -29,7 +29,14 @@ from core.evaluate.rulematcher import (
     prioritize_rules,
 )
 from core import constants as C
+from core import repository
 
+
+def init_repository():
+    repository.initialize('foo')
+
+
+init_repository()
 dummy_config = Configuration(DEFAULT_CONFIG)
 
 

commit 66fc65e1d3c8baa4d574ab96665c4e94bfeb71f0

Fix broken unit tests after 'Repository' changes.
Fixes a bug introduced in commit ca4779cdb7e6235911c799cf3081fa80669b.

Unit tests did not initialize the 'Repository'.

This commit fixes this by adding an additional 'meowuri_class_map',
local to the function 'map_meowuri_to_source_class'.
There really was no reason for the previous version to reach in to the
'Repository' class to access the objects attribute with the same name.

autonameow/core/repository.py
@@ -372,10 +372,12 @@ def map_meowuri_to_source_class(meowuri, includes=None):
         A list of classes that "could" produce and store data with a MeowURI
         that matches the given MeowURI.
     """
+    meowuri_class_map = meowuri_class_map_dict()
+
     def _search_source_type(key):
-        for k, v in SessionRepository.meowuri_class_map[key].items():
+        for k, v in meowuri_class_map[key].items():
             if meowuri.startswith(k):
-                return SessionRepository.meowuri_class_map[key][k]
+                return meowuri_class_map[key][k]
         return None
 
     if not meowuri: