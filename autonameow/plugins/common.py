d0396d532cb265163b90e20ee730c9d552b07773
Jonas Sjöberg <jomeganas@gmail.com>
2017-11-03 14:15:46 +0100

    Update the 'BasePlugin' class to use new format.

--- autonameow/plugins/common.py
+++ autonameow/plugins/common.py
@@ -22,14 +22,23 @@
 import logging
 
 from core import constants as C
-from core import plugin_handler
+from core import (
+    plugin_handler,
+    types
+)
+
 from core.exceptions import InvalidMeowURIError
 from core.model import MeowURI
+from core.util import sanity
 
 
 class BasePlugin(object):
     # Last part of the full MeowURI ('guessit', 'microsoft_vision', ..)
     MEOWURI_LEAF = C.UNDEFINED_MEOWURI_PART
+
+    # Dictionary with plugin-specific information, keyed by the fields that
+    # the raw source produces. Stores information on types, etc..
+    FIELD_LOOKUP = {}
 
     def __init__(self, display_name=None):
         if display_name:
@@ -60,6 +69,41 @@ class BasePlugin(object):
 
     def __call__(self, source, *args, **kwargs):
         self.execute(source)
+
+    def coerce_field_value(self, field, value):
+        _field_lookup_entry = self.FIELD_LOOKUP.get(field)
+        if not _field_lookup_entry:
+            self.log.debug('Field not in "FIELD_LOOKUP"; "{!s}" with value:'
+                           ' "{!s}" ({!s})'.format(field, value, type(value)))
+            return None
+
+        _coercer = _field_lookup_entry.get('typewrap')
+        if not _coercer:
+            self.log.debug('Coercer unspecified for field; "{!s}" with value:'
+                           ' "{!s}" ({!s})'.format(field, value, type(value)))
+            return None
+
+        sanity.check(isinstance(_coercer, types.BaseType),
+                     msg='Got ({!s}) "{!s}"'.format(type(_coercer), _coercer))
+        wrapper = _coercer
+
+        # TODO: [TD0084] Add handling collections to type wrapper classes.
+        if isinstance(value, list):
+            if not _field_lookup_entry.get('multiple', False):
+                self.log.warning(
+                    'Got list but but "FIELD_LOOKUP["multiple"]" is False for '
+                    'field; "{!s}" with value: "{!s}"'.format(field, value)
+                )
+                return None
+
+        try:
+            coerced = wrapper(value)
+        except types.AWTypeError as e:
+            self.log.debug('Wrapping "{!s}" with value "{!s}" raised '
+                           'AWTypeError: {!s}'.format(field, value, e))
+            return None
+        else:
+            return coerced
 
     @classmethod
     def meowuri_prefix(cls):
