d13e551668724539c52dd6954b010473df3b9fa5
Jonas Sjöberg <jomeganas@gmail.com>
2017-11-17 05:47:36 +0100

    Modify 'unit_runner.sh' to use 'unittest'.
    Replaces 'pytest' with 'unittest' in the unit test runner script.
    
    Using 'pytest' was originally only due to the automatic generation
    of HTML test reports for the course work. Now, there is no need for
    the 'pytest'-specific functionality.
    
    The reason for switching is that 'pytest' did not run a couple of
    hundred of the unit tests. I'm not sure why but I noticed that the
    PyCharm test runner ran a lot more tests than what 'unit_runner.sh'
    did.

--- tests/unit_runner.sh
+++ tests/unit_runner.sh
@@ -77,23 +77,6 @@ else
 fi
 
 
-# Make sure required executables are available.
-if ! command -v "pytest" >/dev/null 2>&1
-then
-    echo "This script requires \"pytest\" to run. Exiting .." 1>&2
-    exit 1
-else
-    # Workaround for pytest crashing when writing something other than stdout ..
-    _pytesthelp="$(pytest --help 2>&1)"
-    if ! grep -q -- '--html' <<< "$_pytesthelp"
-    then
-        echo "This script requires \"pytest-html\" to generate HTML reports." 1>&2
-        echo "Install using pip by executing:  pip3 install pytest-html"
-        exit 1
-    fi
-fi
-
-
 _timestamp="$(date "+%Y-%m-%dT%H%M%S")"
 _unittest_log="${AUTONAMEOW_TESTRESULTS_DIR}/unittest_log_${_timestamp}.html"
 if [ -e "$_unittest_log" ]
@@ -103,15 +86,18 @@ then
 fi
 
 
-run_pytest()
+run_unittest()
 {
-    _pytest_report_opts=''
-    [ "$option_write_report" != 'true' ] || _pytest_report_opts="--self-contained-html --html="${_unittest_log}""
-    ( cd "$AUTONAMEOW_ROOT_DIR" && PYTHONPATH=autonameow:tests pytest ${_pytest_report_opts} tests/unit_test_*.py )
+    (
+      cd "$AUTONAMEOW_ROOT_DIR" || return 1
+      PYTHONPATH=autonameow:tests python3 -m unittest discover --catch --buffer --start-directory tests --pattern "unit_test_*.py" --top-level-directory .
+    )
 }
 
+PYTHONPATH=autonameow:tests python3 -m unittest
+
 count_fail=0
-run_task "$option_quiet" "Running \"pytest\"" run_pytest
+run_task "$option_quiet" "Running \"unittest\"" run_unittest
 
 
 if [ -s "$_unittest_log" ]
@@ -126,39 +112,3 @@ then
     exit 0
 fi
 
-
-
-
-# if ! command -v "aha" >/dev/null 2>&1
-# then
-#     logmsg "The executable \"aha\" is not available on this system"
-#     # logmsg "Skipping converting raw logfiles to HTML .."
-#     # exit 1
-# fi
-
-
-# _unittest_rawlog="${AUTONAMEOW_TESTRESULTS_DIR}/unit_stdout_${_timestamp}.raw"
-# if [ -e "$_unittest_rawlog" ]
-# then
-#     echo "File exists: \"${_unittest_rawlog}\" .. Aborting" >&2
-#     exit 1
-# fi
-
-
-# ( cd "$AUTONAMEOW_ROOT_DIR" && PYTHONPATH=autonameow:tests pytest --verbose --color=yes 2>&1 >> "$_unittest_rawlog")
-
-# if [ -s "$_unittest_rawlog" ]
-# then
-#     _unittest_rawlog_html="${_unittest_rawlog}.html"
-#
-#     if aha --title "autonameow Unit Test Log ${_timestamp}" \
-#         < "$_unittest_rawlog" > "$_unittest_rawlog_html"
-#     then
-#         if [ -s "$_unittest_rawlog_html" ]
-#         then
-#             logmsg "Wrote HTML log file: \"${_unittest_rawlog_html}\""
-#         fi
-#     else
-#         logmsg 'FAILED to write HTML log file!'
-#     fi
-# fi
