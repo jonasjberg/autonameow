2c90b6745e7ceae80c8dfed64f9366130864a47c
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-09 13:48:02 +0100

    Remove old, skipped unit tests.

--- tests/unit/test_analyze_filename.py
+++ tests/unit/test_analyze_filename.py
@@ -61,21 +61,6 @@ class TestFilenameAnalyzerWithImageFile(TestCase):
         dt_list = self.fna.get_datetime()
         self.assertIsNotNone(dt_list)
 
-    def test_get_datetime_contains_special_case(self):
-        self.skipTest('TODO: Clean up old code ..')
-        dt_special, = filter(lambda dt: dt['source'] == 'very_special_case',
-                             self.fna.get_datetime())
-        self.assertIsNotNone(dt_special)
-
-    def test_get_datetime_special_case_is_valid(self):
-        self.skipTest('TODO: Clean up old code ..')
-        dt_special, = filter(lambda dt: dt['source'] == 'very_special_case',
-                             self.fna.get_datetime())
-
-        actual = dt_special.get('value')
-        expected = datetime.strptime('20100131 161251', '%Y%m%d %H%M%S')
-        self.assertEqual(actual, expected)
-
 
 class TestLikelyExtension(TestCase):
     def setUp(self):
