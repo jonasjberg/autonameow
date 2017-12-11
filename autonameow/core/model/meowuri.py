0c6454d45eabefbc8b1b19f65585530f0934e3b8
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-11 16:22:01 +0100

    Remove redundant 'MeowURI.__ne__()'.

--- autonameow/core/model/meowuri.py
+++ autonameow/core/model/meowuri.py
@@ -252,9 +252,6 @@ class MeowURI(object):
         else:
             return False
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
     def __lt__(self, other):
         if isinstance(other, self.__class__):
             sp = len(self.parts)
