b1a197275d45ad5c8e67561fb96796308d612f11
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-24 14:41:28 +0100

    Add unit tests in for 'core/constants.py'.

--- tests/unit/test_constants.py
+++ tests/unit/test_constants.py
@@ -39,16 +39,16 @@ class TestConstants(TestCase):
         self.assertTrue(uu.is_internalbytestring(test_input))
         self.assertTrue(uu.is_abspath(test_input))
 
-    def test_constants_contains_program_exit_codes(self):
+    def test_program_exit_codes(self):
         self._is_defined_type(int, C.EXIT_SUCCESS)
         self._is_defined_type(int, C.EXIT_WARNING)
         self._is_defined_type(int, C.EXIT_SANITYFAIL)
         self._is_defined_type(int, C.EXIT_ERROR)
 
-    def test_constants_contains_default_rule_ranking_bias(self):
+    def test_default_rule_ranking_bias(self):
         self._is_defined_type(float, C.DEFAULT_RULE_RANKING_BIAS)
 
-    def test_constants_contains_default_file_tags_options(self):
+    def test_default_file_tags_options(self):
         self._is_defined_internal_string(
             C.DEFAULT_FILETAGS_BETWEEN_TAG_SEPARATOR
         )
@@ -56,24 +56,42 @@ class TestConstants(TestCase):
             C.DEFAULT_FILETAGS_FILENAME_TAG_SEPARATOR
         )
 
-    def test_constants_contains_default_postprocessing_options(self):
+    def test_default_postprocessing_options(self):
         self._is_defined_type(bool, C.DEFAULT_POSTPROCESS_SANITIZE_FILENAME)
         self._is_defined_type(bool, C.DEFAULT_POSTPROCESS_SANITIZE_STRICT)
         self._is_defined_type(bool, C.DEFAULT_POSTPROCESS_LOWERCASE_FILENAME)
         self._is_defined_type(bool, C.DEFAULT_POSTPROCESS_UPPERCASE_FILENAME)
         self._is_defined_type(bool, C.DEFAULT_POSTPROCESS_SIMPLIFY_UNICODE)
 
-    def test_constants_contains_python_version(self):
+    def test_python_version_string(self):
         self._is_defined_internal_string(C.STRING_PYTHON_VERSION)
 
-    def test_constants_contains_analysis_results_fields(self):
+    def test_analysis_results_fields(self):
         self._is_defined_type(list, C.ANALYSIS_RESULTS_FIELDS)
 
-    def test_default_cache_path(self):
+    def test_default_persistence_path(self):
         self._is_defined_absolute_path(C.DEFAULT_PERSISTENCE_DIR_ABSPATH)
 
     def test_default_history_path(self):
+        self._is_defined_type(bytes, C.DEFAULT_HISTORY_FILE_BASENAME)
         self._is_defined_absolute_path(C.DEFAULT_HISTORY_FILE_ABSPATH)
 
     def test_default_cache_max_filesize(self):
         self._is_defined_type(int, C.DEFAULT_CACHE_MAX_FILESIZE)
+
+    def test_default_datetime_formats(self):
+        self._is_defined_internal_string(C.DEFAULT_DATETIME_FORMAT_DATETIME)
+        self._is_defined_internal_string(C.DEFAULT_DATETIME_FORMAT_DATE)
+        self._is_defined_internal_string(C.DEFAULT_DATETIME_FORMAT_TIME)
+
+    def test_default_filesystem_ignores(self):
+        self._is_defined_type(frozenset, C.DEFAULT_FILESYSTEM_IGNORE)
+        self._is_defined_type(frozenset, C.DEFAULT_FILESYSTEM_IGNORE_VCS)
+        self._is_defined_type(frozenset, C.DEFAULT_FILESYSTEM_IGNORE_DARWIN)
+        self._is_defined_type(frozenset, C.DEFAULT_FILESYSTEM_IGNORE_LINUX)
+        self._is_defined_type(frozenset, C.DEFAULT_FILESYSTEM_IGNORE_WINDOWS)
+
+    def test_default_year_limits(self):
+        from datetime import datetime
+        self._is_defined_type(datetime, C.YEAR_UPPER_LIMIT)
+        self._is_defined_type(datetime, C.YEAR_LOWER_LIMIT)
