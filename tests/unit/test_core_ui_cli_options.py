dbc2e4422ab197222626783aa97b6903f4963caa
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-31 05:37:11 +0100

    Rename 'test_cli_options.py' to 'test_core_ui_cli_options.py'.

--- tests/unit/test_core_ui_cli_options.py
+++ tests/unit/test_core_ui_cli_options.py
@@ -22,13 +22,13 @@
 import argparse
 from unittest import TestCase
 
+import unit.utils as uu
 from core.ui.cli.options import (
     arg_is_year,
     arg_is_readable_file,
     init_argparser,
     cli_parse_args
 )
-import unit.utils as uu
 
 
 class TestArgumentValidators(TestCase):
