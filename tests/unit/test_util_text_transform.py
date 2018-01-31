1554da77506c380e54f667ad69b8d0c114dc2f59
Jonas Sjöberg <jomeganas@gmail.com>
2018-01-31 19:15:47 +0100

    Skip unit test if 'unidecode' is unavailable.

--- tests/unit/test_util_text_transform.py
+++ tests/unit/test_util_text_transform.py
@@ -19,7 +19,14 @@
 #   You should have received a copy of the GNU General Public License
 #   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
 
-from unittest import TestCase
+from unittest import TestCase, skipIf
+
+try:
+    import guessit
+except ImportError:
+    UNIDECODE_IS_NOT_AVAILABLE = True, 'Missing required module "unidecode"'
+else:
+    UNIDECODE_IS_NOT_AVAILABLE = False, ''
 
 import unit.utils as uu
 from core.exceptions import EncodingBoundaryViolation
@@ -447,6 +454,7 @@ class TestStripAccentsHomeRolled(TestCase):
                             expect='Mere, Francoise, noel, 889')
 
 
+@skipIf(*UNIDECODE_IS_NOT_AVAILABLE)
 class TestStripAccentsUnidecode(TestCase):
     def _assert_strips(self, given, expect):
         actual = _strip_accents_unidecode(given)
