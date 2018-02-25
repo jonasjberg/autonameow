3450c78d8b3ecbbb1b2654188a9680a971cafe10
Jonas Sjöberg <jomeganas@gmail.com>
2018-02-25 10:02:22 +0100

    Add 'devscripts/find_unit_tests_visits.sh'.

---
+++ devscripts/find_unit_test_visits.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#   Copyright(c) 2016-2018 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+# Usage: devscripts/find_unit_tests_visits.sh autonameow/core/master_provider
+
+PYTHONPATH=autonameow:tests py.test --smother autonameow tests/unit/test_*.py
+PYTHONPATH=autonameow:tests smother lookup "$@"
