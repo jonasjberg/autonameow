47d007632aa9695eb8163113d663a18e5d3c5620
Jonas Sjöberg <jomeganas@gmail.com>
2018-04-04 07:26:17 +0200

    Add regular expression caching 'regexcache.py'.
    Adds a new source file with a singleton class for regex caching.
    
    Intended to replace compiling module-global regular expressions
    when loading source files with compiling once at first use and
    then re-use the same cached compiled regular expression objects.

---
+++ tests/unit/test_util_text_regexcache.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2018 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from util.text.regexcache import (
+    _RegexCache,
+)
+
+
+class TestRegexCache(TestCase):
+    def test_instantiated_regexcache_is_not_none(self):
+        rc = _RegexCache()
+        self.assertIsNotNone(rc)
