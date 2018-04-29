a78177b6a02a102d98e5ca8a5e3ebd44df4b60d1
Jonas Sjöberg <jomeganas@gmail.com>
2018-04-29 21:56:54 +0200

    Rework string value canonicalization.
    Various rework related to the 'StringValueCanonicalizer'.
    
    Modifies the format of the config yaml file.
    Patterns are now either literal strings or regular expressions.
    
    Example yaml config section:
    
        CanonicalValue:
            match_any_regex:
            - regex pattern 1
            - regex pattern 2
            match_any_literal:
            - string pattern 1
            - string pattern 2
    
    Any value that matches either 'CanonicalValue', any of the
    regular expressions or any of the literal strings is replaced
    with 'CanonicalValue'.
    
    The "parsing" of the regular expressions and literals is moved
    to a separate class 'CanonicalizerConfigParser' so that the
    'StringValueCanonicalizer' class can be passed plain dicts keyed
    by canonical values, storing lists of either literal strings or
    compiled regular expressions.
    
    This also adds handling of matching multiple regular expressions,
    in which case the canonical value whose regular expression produced
    the longest matched substring is used.

--- autonameow/core/metadata/canonicalize.py
+++ autonameow/core/metadata/canonicalize.py
@@ -22,6 +22,7 @@
 import logging
 import os
 import re
+from collections import defaultdict
 
 from util import coercers
 from util import disk
@@ -42,60 +43,149 @@ def _relative_absolute_path(basename):
 
 
 class StringValueCanonicalizer(object):
-    def __init__(self, value_lookup_dict, lookup_dict_filepath=None):
-        if lookup_dict_filepath:
-            str_lookup_dict_filepath = coercers.force_string(lookup_dict_filepath)
-        else:
-            str_lookup_dict_filepath = '(unknown filepath)'
-
-        # Store original path for additional information when logging errors.
-        self.str_lookup_dict_filepath = str(str_lookup_dict_filepath)
-        self.canonical_value_regexes = self._parse_canonical_value_regexes(value_lookup_dict)
+    def __init__(self, literal_lookup_dict, regex_lookup_dict):
+        assert isinstance(literal_lookup_dict, dict)
+        assert isinstance(regex_lookup_dict, dict)
+        self.canonical_value_literals = literal_lookup_dict
+        self.canonical_value_regexes = regex_lookup_dict
 
     def __call__(self, string):
-        canonical_value = self._find_first_match(string)
+        """
+        Returns a canonical representation if the given value if found.
+        If no canonical value is found, the given value is returned as-is.
+        """
+        canonical_value = self._find_canonical_value(string)
         if canonical_value is None:
             return string
         return canonical_value
 
-    def _find_first_match(self, string):
-        lowercase_string = string.lower()
+    def _find_canonical_value(self, string):
+        # Check if the given value is equal to any canonical value when
+        # disregarding differences in letter case.
+        all_canonical_values = set(self.canonical_value_regexes.keys())
+        all_canonical_values.update(self.canonical_value_literals.keys())
+        matched_canonical_value = self._ignored_case_matches_any(string, all_canonical_values)
+        if matched_canonical_value:
+            return matched_canonical_value
 
-        for canonical_value, regexes in self.canonical_value_regexes.items():
-            if lowercase_string == canonical_value.lower():
+        # Do literal string comparison.
+        for canonical_value, literals in self.canonical_value_literals.items():
+            if any(string == lit for lit in literals):
                 return canonical_value
 
-            if any(regex.match(string) for regex in regexes):
+        # Do regex matching.
+        # Use canonical form with longest total length of matched substrings.
+        matches = defaultdict(int)
+        for canonical_value, regexes in self.canonical_value_regexes.items():
+            for regex in regexes:
+                matchiter = re.finditer(regex, string)
+                for match in matchiter:
+                    matched_substring = match.group(0)
+                    matches[canonical_value] += len(matched_substring)
+
+        if matches:
+            canonical_form_of_longest_match = max(matches, key=matches.get)
+            return canonical_form_of_longest_match
+
+        return None
+
+    def _ignored_case_matches_any(self, value, canonical_values):
+        lowercase_value = value.lower()
+
+        for canonical_value in canonical_values:
+            if lowercase_value == canonical_value.lower():
                 return canonical_value
 
         return None
 
-    def _parse_canonical_value_regexes(self, datadict):
-        assert isinstance(datadict, dict)
 
-        parsed_value_regexes = dict()
-        for canonical, pattern_list in datadict.items():
-            compiled_regexes = list()
+class CanonicalizerConfigParser(object):
+    CONFIG_SECTION_MATCH_ANY_LITERAL = 'match_any_literal'
+    CONFIG_SECTION_MATCH_ANY_REGEX = 'match_any_regex'
 
-            for pattern in pattern_list:
-                try:
-                    regex = re.compile(pattern, re.DOTALL | re.IGNORECASE)
-                except re.error as e:
-                    log.error('Invalid regex in "{!s}" :: '
-                              '{!s}'.format(self.str_lookup_dict_filepath, e))
-                    continue
-                compiled_regexes.append(regex)
+    def __init__(self, config_datadict, lookup_dict_filepath=None):
+        assert isinstance(config_datadict, dict)
+        self._datadict = config_datadict
 
-            if compiled_regexes:
-                parsed_value_regexes[canonical] = compiled_regexes
+        # Store original path for additional information when logging errors.
+        if lookup_dict_filepath:
+            str_lookup_dict_filepath = coercers.force_string(lookup_dict_filepath)
+        else:
+            str_lookup_dict_filepath = '(unknown)'
+        self.str_lookup_dict_filepath = str(str_lookup_dict_filepath)
 
-        return parsed_value_regexes
+        self._parsed_literal_lookup = None
+        self._parsed_regex_lookup = None
+
+    @property
+    def parsed_literal_lookup(self):
+        if self._parsed_literal_lookup is None:
+            self._parsed_literal_lookup = self._parse_literal_lookup()
+        return self._parsed_literal_lookup
+
+    @property
+    def parsed_regex_lookup(self):
+        if self._parsed_regex_lookup is None:
+            self._parsed_regex_lookup = self._parse_regex_lookup()
+        return self._parsed_regex_lookup
+
+    def _parse_literal_lookup(self):
+        literal_lookup = dict()
+
+        for canonical_form, sections in self._datadict.items():
+            assert isinstance(canonical_form, str)
+            if not canonical_form.strip():
+                continue
+
+            literals_to_match = sections.get(self.CONFIG_SECTION_MATCH_ANY_LITERAL)
+            if not literals_to_match:
+                continue
+
+            valid_literals_to_match = [s for s in literals_to_match if s and s.strip()]
+            if valid_literals_to_match:
+                literal_lookup[canonical_form] = set(valid_literals_to_match)
+
+        return literal_lookup
+
+    def _parse_regex_lookup(self):
+        regex_lookup = dict()
+
+        for canonical_form, sections in self._datadict.items():
+            assert isinstance(canonical_form, str)
+            if not canonical_form.strip():
+                continue
+
+            regexes_to_match = sections.get(self.CONFIG_SECTION_MATCH_ANY_REGEX)
+            if not regexes_to_match:
+                continue
+
+            non_empty_regex_patterns = [s for s in regexes_to_match if s and s.strip()]
+            if non_empty_regex_patterns:
+                compiled_regexes = set()
+                for pattern in non_empty_regex_patterns:
+                    try:
+                        regex = re.compile(pattern, re.IGNORECASE)
+                    except re.error as e:
+                        log.error('Invalid regex pattern "{!s}" in "{!s}" :: '
+                                  '{!s}'.format(pattern, self.str_lookup_dict_filepath, e))
+                        continue
+                    compiled_regexes.add(regex)
+
+                if compiled_regexes:
+                    regex_lookup[canonical_form] = compiled_regexes
+
+        return regex_lookup
 
 
 def build_string_value_canonicalizer(yaml_config_filename):
     yaml_config_filepath = _relative_absolute_path(yaml_config_filename)
     config_data = disk.load_yaml_file(yaml_config_filepath)
-    canonicalizer = StringValueCanonicalizer(config_data, yaml_config_filepath)
+
+    parser = CanonicalizerConfigParser(config_data, yaml_config_filepath)
+    literal_lookup_dict = parser.parsed_literal_lookup
+    regex_lookup_dict = parser.parsed_regex_lookup
+
+    canonicalizer = StringValueCanonicalizer(literal_lookup_dict, regex_lookup_dict)
     return canonicalizer
 
 
--- tests/unit/test_core_metadata_canonicalize.py
+++ tests/unit/test_core_metadata_canonicalize.py
@@ -19,13 +19,15 @@
 #   You should have received a copy of the GNU General Public License
 #   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
 
+import re
 from unittest import TestCase
 
 import unit.utils as uu
 from core.metadata.canonicalize import (
     build_string_value_canonicalizer,
     canonicalize_publisher,
-    StringValueCanonicalizer
+    StringValueCanonicalizer,
+    CanonicalizerConfigParser
 )
 
 
@@ -35,6 +37,10 @@ def _canonicalize_publisher(given):
 
 def _build_string_value_canonicalizer(*args, **kwargs):
     return build_string_value_canonicalizer(*args, **kwargs)
+
+
+def _get_canonicalizer_config_parser(*args, **kwargs):
+    return CanonicalizerConfigParser(*args, **kwargs)
 
 
 PUBLISHER_CANONICAL_EQUIVALENTS = {
@@ -142,6 +148,7 @@ PUBLISHER_CANONICAL_EQUIVALENTS = {
         'McGraw Hill Professional',
         'McGraw-Hill',
         'McGrawHill',
+        'McGraw Hill',
         'The McGraw-Hill Companies',
     ],
     'MicrosoftPress': [
@@ -153,11 +160,16 @@ PUBLISHER_CANONICAL_EQUIVALENTS = {
         'MIT Press',
         'MITPress',
         'mitpress.mit.edu',
+        'M I T Press',
     ],
     'MorganKaufmann': [
         'Morgan Kaufmann Publishers',
         'Morgan Kaufmann',
         'MorganKaufmann',
+    ],
+    'MorganClaypool': [
+        'Morgan & Claypool Publishers',
+        'Morgan and Claypool Publishers'
     ],
     'NewAgeInt': [
         'NewAgeInt',
@@ -174,10 +186,10 @@ PUBLISHER_CANONICAL_EQUIVALENTS = {
     'OReilly':  [
         'OReilly',
         'oreilly',
-        "O'Reilly Media"
-        "O'Reilly"
-        'O\u2019Reilly Media, Inc.'
-        "O'Reilly & Associates"
+        "O'Reilly Media",
+        "O'Reilly",
+        'O\u2019Reilly Media, Inc.',
+        "O'Reilly & Associates",
         'Oreilly & Associates Inc',
         'Oreilly & Associates',
     ],
@@ -241,6 +253,7 @@ PUBLISHER_CANONICAL_EQUIVALENTS = {
     'TaylorFrancis': [
         'TaylorFrancis',
         'Taylor and Francis',
+        'Taylor & Francis',
     ],
     'Westview': [
         'Westview',
@@ -276,15 +289,205 @@ PUBLISHER_CANONICAL_EQUIVALENTS = {
 }
 
 
-class TestBuildStringValueCanonicalizer(TestCase):
+class TestCanonicalizerConfigParser(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.SECTION_MATCH_ANY_LITERAL = CanonicalizerConfigParser.CONFIG_SECTION_MATCH_ANY_LITERAL
+        cls.SECTION_MATCH_ANY_REGEX = CanonicalizerConfigParser.CONFIG_SECTION_MATCH_ANY_REGEX
+
+    def _get_parser_from_empty_config(self):
+        empty_config = dict()
+        return _get_canonicalizer_config_parser(empty_config)
+
+    def _get_parser_from_config(self, config):
+        return _get_canonicalizer_config_parser(config)
+
+    def _compile_regex(self, pattern):
+        # NOTE: Compilation flags must match those used in the implementation.
+        return re.compile(pattern, re.IGNORECASE)
+
     def test_filepath_config_is_expected_absolute_path(self):
-        c = _build_string_value_canonicalizer(b'canonical_publisher.yaml')
-        actual = c.str_lookup_dict_filepath
+        empty_config = dict()
+        parser = _get_canonicalizer_config_parser(empty_config, b'/tmp/canonical_publisher.yaml')
+        actual = parser.str_lookup_dict_filepath
         self.assertIsInstance(actual, str,
                               '*not bytestring* path is stored for logging only')
         self.assertTrue(actual.endswith('canonical_publisher.yaml'))
         self.assertTrue(uu.is_abspath(actual))
 
+    def test_can_be_instantiated_with_empty_config_dict(self):
+        parser = self._get_parser_from_empty_config()
+        self.assertIsNotNone(parser)
+
+    def test_parsed_literal_lookup_is_empty_when_given_empty_config_dict(self):
+        parser = self._get_parser_from_empty_config()
+        self.assertEqual(dict(), parser.parsed_literal_lookup)
+
+    def test_parsed_regex_lookup_is_empty_when_given_empty_config_dict(self):
+        parser = self._get_parser_from_empty_config()
+        self.assertEqual(dict(), parser.parsed_regex_lookup)
+
+    def test_parsed_literal_lookup_is_empty_when_given_only_empty_strings(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    '',
+                ]
+            },
+            'BarPub': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    ' ',
+                    '  ',
+                ]
+            }
+        })
+        self.assertEqual(dict(), parser.parsed_literal_lookup)
+
+    def test_parsed_literal_lookup_is_empty_when_given_empty_literals(self):
+        parser = self._get_parser_from_config({
+            '': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    'foo',
+                ]
+            },
+            '  ': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    'bar',
+                ]
+            }
+        })
+        self.assertEqual(dict(), parser.parsed_literal_lookup)
+
+    def test_returns_expected_parsed_literal_lookup_given_config_with_one_entry(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    'Foo',
+                    'foo pub',
+                    'Foo',  # Duplicate that should be removed
+                    '   ',  # Whitespace that should be ignored
+                ]
+             }
+        })
+        expect_parsed_literal_lookup = {
+            'FooPub': set([
+                'Foo',
+                'foo pub'
+             ])
+        }
+        self.assertEqual(expect_parsed_literal_lookup, parser.parsed_literal_lookup)
+
+    def test_returns_expected_parsed_literal_lookup_given_config_with_two_entries(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    'Foo',
+                    'foo pub',
+                    'Foo',  # Duplicate that should be removed
+                ]
+             },
+            'BarPub': {
+                self.SECTION_MATCH_ANY_LITERAL: [
+                    'Bar Publishers Inc.',
+                    'bar pub.',
+                    '\n',  # Whitespace that should be ignored
+                ]
+             }
+        })
+        expect_parsed_literal_lookup = {
+            'FooPub': set([
+                'Foo',
+                'foo pub'
+             ]),
+            'BarPub': set([
+                'Bar Publishers Inc.',
+                'bar pub.'
+             ])
+        }
+        self.assertEqual(expect_parsed_literal_lookup, parser.parsed_literal_lookup)
+
+    def test_parsed_regex_lookup_is_empty_when_given_only_empty_strings(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    '',
+                ]
+            },
+            'BarPub': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    ' ',
+                    '  ',
+                ]
+            }
+        })
+        self.assertEqual(dict(), parser.parsed_regex_lookup)
+
+    def test_parsed_regex_lookup_is_empty_when_given_empty_regex(self):
+        parser = self._get_parser_from_config({
+            '': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    'foo',
+                ]
+            },
+            '  ': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    'bar',
+                ]
+            }
+        })
+        self.assertEqual(dict(), parser.parsed_regex_lookup)
+
+    def test_returns_expected_parsed_regex_lookup_given_config_with_one_entry(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    'Foo',
+                    'foo pub',
+                    'Foo',  # Duplicate that should be removed
+                    '   ',  # Whitespace that should be ignored
+                ]
+             }
+        })
+        expect_parsed_regex_lookup = {
+            'FooPub': set([
+                self._compile_regex('Foo'),
+                self._compile_regex('foo pub')
+             ])
+        }
+        self.assertEqual(expect_parsed_regex_lookup, parser.parsed_regex_lookup)
+
+    def test_returns_expected_parsed_regex_lookup_given_config_with_two_entries(self):
+        parser = self._get_parser_from_config({
+            'FooPub': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    'Foo',
+                    'foo pub',
+                    'Foo',  # Duplicate that should be removed
+                ]
+             },
+            'BarPub': {
+                self.SECTION_MATCH_ANY_REGEX: [
+                    'Bar Publishers Inc.',
+                    'bar pub.',
+                    '\n',  # Whitespace that should be ignored
+                ]
+             }
+        })
+        expect_parsed_regex_lookup = {
+            'FooPub': set([
+                self._compile_regex('Foo'),
+                self._compile_regex('foo pub')
+             ]),
+            'BarPub': set([
+                self._compile_regex('Bar Publishers Inc.'),
+                self._compile_regex('bar pub.')
+             ])
+        }
+        self.assertEqual(expect_parsed_regex_lookup, parser.parsed_regex_lookup)
+
+
+
+class TestBuildStringValueCanonicalizer(TestCase):
     def test_returns_given_as_is_when_used_as_callable(self):
         c = _build_string_value_canonicalizer(b'canonical_publisher.yaml')
         actual = c('Manning')
@@ -300,14 +503,18 @@ class TestCanonicalizePublisher(TestCase):
     def test_canonicalize_publisher(self):
         for canonical, equivalent_values in PUBLISHER_CANONICAL_EQUIVALENTS.items():
             for equivalent_value in equivalent_values:
-                with self.subTest(given=equivalent_value):
+                with self.subTest(given_expected=(equivalent_value, canonical)):
                     actual = _canonicalize_publisher(equivalent_value)
                     self.assertEqual(canonical, actual)
 
 
 class TestStringValueCanonicalizer(TestCase):
+    def _compile_regex(self, pattern):
+        # NOTE: Compilation flags must match those used in the implementation.
+        return re.compile(pattern, re.IGNORECASE)
+
     def test_returns_values_as_is_when_given_empty_lookup_data(self):
-        c = StringValueCanonicalizer(value_lookup_dict=dict())
+        c = StringValueCanonicalizer(literal_lookup_dict=dict(), regex_lookup_dict=dict())
 
         for given_and_expected in [
             '',
@@ -318,13 +525,13 @@ class TestStringValueCanonicalizer(TestCase):
                 actual = c(given_and_expected)
                 self.assertEqual(given_and_expected, actual)
 
-    def test_replaces_one_value_matching_one_pattern(self):
-        value_lookup_dict = {
+    def test_replaces_one_value_matching_one_regex_pattern(self):
+        regex_lookup_dict = {
             'BAZ': [
-                '.*foo'
+                self._compile_regex('.*foo')
             ]
         }
-        c = StringValueCanonicalizer(value_lookup_dict)
+        c = StringValueCanonicalizer(literal_lookup_dict=dict(), regex_lookup_dict=regex_lookup_dict)
 
         for given in [
             'foo',
@@ -341,29 +548,27 @@ class TestStringValueCanonicalizer(TestCase):
                 actual = c(given)
                 self.assertEqual('BAZ', actual)
 
-    def test_replaces_two_values_matching_one_pattern_each(self):
-        value_lookup_dict = {
+    def test_replaces_two_values_matching_one_regex_pattern_each(self):
+        regex_lookup_dict = {
             'BAZ': [
-                '.*foo.*'
+                self._compile_regex('foo')
             ],
             'MEOW': [
-                '.*w.*'
+                self._compile_regex('w')
             ]
         }
-        c = StringValueCanonicalizer(value_lookup_dict)
+        c = StringValueCanonicalizer(literal_lookup_dict=dict(), regex_lookup_dict=regex_lookup_dict)
 
         for given, expected in [
-            ('BAZ', 'BAZ'),
-            ('baz', 'BAZ'),
-            ('foo', 'BAZ'),
+            ('BAZ',     'BAZ'),
+            ('baz',     'BAZ'),
+            ('foo',     'BAZ'),
             ('foo foo', 'BAZ'),
 
             ('MEOW', 'MEOW'),
             ('meow', 'MEOW'),
-            ('MEOW MEOW', 'MEOW'),
-            ('meow meow', 'MEOW'),
-            ('w', 'MEOW'),
-            ('ww', 'MEOW'),
+            ('w',    'MEOW'),
+            ('ww',   'MEOW'),
 
             # Patterns for both 'BAZ' and 'MEOW' match.
             # TODO: [incomplete] Result depends on order in 'value_lookup_dict'.. ?
@@ -371,7 +576,7 @@ class TestStringValueCanonicalizer(TestCase):
             ('foo w', 'BAZ'),
             ('w foo', 'BAZ'),
         ]:
-            with self.subTest():
+            with self.subTest(given_expected=(given, expected)):
                 actual = c(given)
                 self.assertEqual(expected, actual)
 
@@ -383,3 +588,106 @@ class TestStringValueCanonicalizer(TestCase):
             with self.subTest():
                 actual = c(given_and_expected)
                 self.assertEqual(given_and_expected, given_and_expected)
+
+    def test_uses_value_with_longest_match_in_case_of_conflicts(self):
+        regex_lookup_dict = {
+            'FOO': [
+                self._compile_regex('F+')
+            ],
+            'BAR': [
+                self._compile_regex('B+')
+            ]
+        }
+        c = StringValueCanonicalizer(literal_lookup_dict=dict(), regex_lookup_dict=regex_lookup_dict)
+
+        for given, expected in [
+            ('F',     'FOO'),
+            ('FF',    'FOO'),
+            ('FFB',   'FOO'),
+            ('BFF',   'FOO'),
+            # ('FFBB',  'FOO'),  # TODO: Handle tied match lengths?
+            ('B',     'BAR'),
+            ('BB',    'BAR'),
+            ('BBF',   'BAR'),
+            ('FBB',   'BAR'),
+            # ('BBFF',  'BAR'),  # TODO: Handle tied match lengths?
+        ]:
+            with self.subTest(given_expected=(given, expected)):
+                actual = c(given)
+                self.assertEqual(expected, actual)
+
+    def test_uses_value_with_longest_match_in_case_of_conflicts_multiple_patterns(self):
+        regex_lookup_dict = {
+            'FOO': [
+                self._compile_regex('f+'),
+                self._compile_regex('x+')
+            ],
+            'BAR': [
+                self._compile_regex('b+'),
+                self._compile_regex('y+')
+            ]
+        }
+        c = StringValueCanonicalizer(literal_lookup_dict=dict(), regex_lookup_dict=regex_lookup_dict)
+
+        for given, expected in [
+            ('f',     'FOO'),
+            ('ff',    'FOO'),
+            ('x',     'FOO'),
+            ('xx',    'FOO'),
+            ('fx',    'FOO'),
+            ('xf',    'FOO'),
+            ('xxff',  'FOO'),
+            ('ffxx',  'FOO'),
+
+            ('b',     'BAR'),
+            ('bb',    'BAR'),
+            ('y',     'BAR'),
+            ('yy',    'BAR'),
+            ('by',    'BAR'),
+            ('yb',    'BAR'),
+            ('yybb',  'BAR'),
+            ('bbyy',  'BAR'),
+
+            ('ffb',   'FOO'),
+            ('bff',   'FOO'),
+            ('ffy',   'FOO'),
+            ('yff',   'FOO'),
+
+            ('fbb',   'BAR'),
+            ('bbf',   'BAR'),
+            ('bbx',   'BAR'),
+            ('xbb',   'BAR'),
+
+            # ('ffBB',  'FOO'),  # TODO: Handle tied match lengths?
+            # ('bbFF',  'BAR'),  # TODO: Handle tied match lengths?
+
+            ('ffff bb',  'FOO'),
+            ('fxfx bb',  'FOO'),
+            ('fxxx bb',  'FOO'),
+            ('xxxx bb',  'FOO'),
+
+            ('bbbb ff',  'BAR'),
+            ('byyy ff',  'BAR'),
+            ('yyyy ff',  'BAR'),
+
+            ('bb ffff',  'FOO'),
+            ('ff ff bb', 'FOO'),
+            ('bb ff ff', 'FOO'),
+
+            ('ff bbb',  'BAR'),
+            ('ff bbbb', 'BAR'),
+            ('bbb ff',  'BAR'),
+            ('bbbb ff', 'BAR'),
+
+            ('ffff _ bb',   'FOO'),
+            ('ffff _ bb _', 'FOO'),
+            ('bb _ ffff',   'FOO'),
+            ('ff _ ff bb',  'FOO'),
+            ('bb _ ffff',   'FOO'),
+            ('_ bb _ ffff', 'FOO'),
+            ('_ fff _ bb',  'FOO'),
+            ('_ bb _ ffff', 'FOO'),
+        ]:
+            with self.subTest(given_expected=(given, expected)):
+                actual = c(given)
+                self.assertEqual(expected, actual)
