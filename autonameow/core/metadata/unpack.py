c252d146ba2a4a6a35a7ad37d39e1b39d7aedec5
Jonas Sjöberg <jomeganas@gmail.com>
2018-05-12 22:33:29 +0200

    Add (WIP) file 'core/metadata/unpack.py' and tests.
    Adds (WIP) functionality for handling titles with embedded subtitle.
    
    References TODO [TD0191] on detecting and extracting subtitles.

---
+++ autonameow/core/metadata/unpack.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2018 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
+
+# TODO: [TD0191] Detect and extract subtitles from titles
+# TODO: [TD0192] Detect and extract editions from titles
+
+
+def unpack_field_value(todo):
+    #
+    #   URI:  extractor.metadata.exiftool.XMP:Rights
+    # VALUE:  ""Copyright © 2012 "
+    #
+    # Should unpack to a "date_created" (or similar) with value 2012.
+    pass
+
+
+def split_title_subtitle(full_title):
+    assert isinstance(full_title, str)
+
+    RE_SEPARATORS = r'[;:-]'
+    RE_TRAILING_SEPARATORS = r'[;:-]$'
+
+    parts = re.split(RE_SEPARATORS, full_title)
+    stripped_parts = [p.strip() for p in parts]
+
+    if len(stripped_parts) == 1:
+        return stripped_parts[0], ''
+
+    if len(stripped_parts) > 2:
+        # Pluck the subtitle from 'stripped_parts'
+        subtitle = stripped_parts[-1]
+        del stripped_parts[-1]
+
+        # Remove the subtitle from the full title and clean it up.
+        title = full_title.replace(subtitle, '').strip()
+        title_without_trailing_seps = re.sub(RE_TRAILING_SEPARATORS, '', title)
+
+        return title_without_trailing_seps, subtitle
+
+    return stripped_parts
---
+++ tests/unit/test_core_metadata_unpack.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2018 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of autonameow.
+#
+#   autonameow is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   autonameow is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+from core.metadata.unpack import split_title_subtitle
+
+
+class TestSplitTitleSubtitle(TestCase):
+    def _assert_splits(self, full_title, title, subtitle):
+        actual_title, actual_subtitle = split_title_subtitle(full_title)
+        self.assertEqual(title, actual_title)
+        self.assertEqual(subtitle, actual_subtitle)
+
+    def test_handles_empty_titles(self):
+        self._assert_splits('', '', '')
+
+    def test_handles_trivial_titles(self):
+        self._assert_splits('foo', title='foo', subtitle='')
+        self._assert_splits('foo bar', title='foo bar', subtitle='')
+
+    def test_splits_title_with_subtitle_separated_by_single_semicolon(self):
+        for given_title in [
+            'Practical Meow with Gibson ; Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson ;Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson; Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson;Unleash the power of Meowing and unlock your creativity',
+        ]:
+            self._assert_splits(
+                given_title,
+                title='Practical Meow with Gibson',
+                subtitle='Unleash the power of Meowing and unlock your creativity'
+            )
+
+    def test_splits_title_with_subtitle_separated_by_single_colon(self):
+        for given_title in [
+            'Practical Meow with Gibson : Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson :Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson: Unleash the power of Meowing and unlock your creativity',
+            'Practical Meow with Gibson:Unleash the power of Meowing and unlock your creativity',
+        ]:
+            self._assert_splits(
+                given_title,
+                title='Practical Meow with Gibson',
+                subtitle='Unleash the power of Meowing and unlock your creativity'
+            )
+
+    def test_splits_title_with_subtitle_separated_by_single_dash(self):
+        for given_title in [
+            'X Essentials - Programming the X Framework',
+            'X Essentials -Programming the X Framework',
+            'X Essentials- Programming the X Framework',
+            'X Essentials-Programming the X Framework',
+        ]:
+            self._assert_splits(
+                given_title,
+                title='X Essentials',
+                subtitle='Programming the X Framework'
+            )
+
+    def test_splits_full_title_containing_two_possible_separators(self):
+            self._assert_splits(
+                'Meowing for Computations - MEOW: A Gentle Introduction to Numerical Simulation with Gibson',
+                title='Meowing for Computations - MEOW',
+                subtitle='A Gentle Introduction to Numerical Simulation with Gibson'
+            )
