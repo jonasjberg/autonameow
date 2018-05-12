jonasjberg/autonameow

commit eeaf4866a123970f23f0ad6db7ee0e0c36878a35

Add replacing Unicode hyphen and unit tests.

autonameow/util/text/transform.py
@@ -191,6 +191,7 @@ def indent(text, columns=None, padchar=None):
     return ''.join(padding + line for line in text.splitlines(True))
 
 
+# \u00AD Soft hyphen
 # \u002D Hyphen-minus
 # \u05BE Hebrew punctuation MAQAF
 # \u2010 Hyphen
@@ -202,7 +203,7 @@ def indent(text, columns=None, padchar=None):
 # \u2043 Hyphen bullet
 # \u30FB Katakana middle dot
 RE_UNICODE_DASHES_HYPHENS = re.compile(
-    '[\u002d\u05be\u2010\u2011\u2012\u2013\u2014\u2015\u2043\u30fb]'
+    '[\u00AD\u002d\u05be\u2010\u2011\u2012\u2013\u2014\u2015\u2043\u30fb]'
 )
 
 # \u002D Hyphen-minus

tests/unit/test_util_text_transform.py
@@ -550,6 +550,20 @@ class TestNormalizeUnicode(TestCase):
         self._aE('\u0305', '-')
         self._aE('\u203e', '-')
 
+    def test_replaces_hyphens(self):
+        self._aE('\u002D', '-')
+        self._aE('\u00AD', '-')
+        self._aE('\u00AD', '-')
+        self._aE('\u05BE', '-')
+        self._aE('\u2010', '-')
+        self._aE('\u2011', '-')
+        self._aE('\u2012', '-')
+        self._aE('\u2013', '-')
+        self._aE('\u2014', '-')
+        self._aE('\u2015', '-')
+        self._aE('\u2043', '-')
+        self._aE('\u30FB', '-')
+
 
 class TestRemoveNonBreakingSpaces(TestCase):
     def test_remove_non_breaking_spaces_removes_expected(self):

commit e0df265a48643a1ec2f7a68dc432554b701470c5

Fix regression in replacement of Unicode hyphens.
Fixes bug introduced in eeaf4866a123970f23f0ad6db7ee0e0c36878a35

This commit added replacing Unicode '\u00AD' with '-', which also
caused unintentional replacements of 'ö' characters.

This is now fixed by using the Python escape '\xad' in place of
'\u00AD' for "soft hyphens".

autonameow/util/text/transform.py
@@ -203,7 +203,7 @@ def indent(text, columns=None, padchar=None):
 # \u2043 Hyphen bullet
 # \u30FB Katakana middle dot
 RE_UNICODE_DASHES_HYPHENS = re.compile(
-    '[\u00AD\u002d\u05be\u2010\u2011\u2012\u2013\u2014\u2015\u2043\u30fb]'
+    r'[\xad\u002d\u05be\u2010\u2011\u2012\u2013\u2014\u2015\u2043\u30fb]'
 )
 
 # \u002D Hyphen-minus