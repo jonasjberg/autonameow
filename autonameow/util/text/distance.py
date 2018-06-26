783a3cc3e174cd0031730584f00f8a965659dc63
Jonas Sjöberg <jomeganas@gmail.com>
2018-06-26 21:57:38 +0200

    Add common substring length functions.
    Adds functions for calculating lengths of common substrings.

--- autonameow/util/text/distance.py
+++ autonameow/util/text/distance.py
@@ -19,6 +19,8 @@
 #   You should have received a copy of the GNU General Public License
 #   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
 
+from difflib import SequenceMatcher
+
 
 def levenshtein(string_a, string_b):
     n, m = len(string_a), len(string_b)
@@ -44,6 +46,24 @@ def normalized_levenshtein(string_a, string_b):
     lev_dist = levenshtein(string_a, string_b)
     normalized_dist = lev_dist / max(1, max(len(string_a), len(string_b)))
     return normalized_dist
+
+
+def longest_common_substring_length(string_a, string_b):
+    assert isinstance(string_a, str) and isinstance(string_b, str)
+
+    matcher = SequenceMatcher(None, string_a, string_b, autojunk=False)
+    match = matcher.find_longest_match(0, len(string_a), 0, len(string_b))
+    return int(match.size)
+
+
+def total_common_substring_length(string_a, string_b):
+    assert isinstance(string_a, str), (type(string_a), str(string_a))
+    assert isinstance(string_b, str), (type(string_b), str(string_b))
+
+    matcher = SequenceMatcher(None, string_a, string_b, autojunk=False)
+    matches = matcher.get_matching_blocks()
+    total_matched_length = sum(m.size for m in matches)
+    return total_matched_length
 
 
 def string_difference(string_a, string_b):
@@ -76,14 +96,16 @@ if __name__ == '__main__':
     for a, b in arg_combinations:
         s = string_similarity(a, b)
         d = string_difference(a, b)
-        results.append((a, b, s, d))
+        lcsl = longest_common_substring_length(a, b)
+        tcsl = total_common_substring_length(a, b)
+        results.append((a, b, s, d, lcsl, tcsl))
 
     from core.view.cli.common import ColumnFormatter
     cf = ColumnFormatter()
-    cf.addrow('String A', 'String B', 'Similarity', 'Difference')
-    cf.addrow('========', '========', '==========', '==========')
+    cf.addrow('String A', 'String B', 'Similarity', 'Difference', 'LCSL', 'TCSL')
+    cf.addrow('========', '========', '==========', '==========', '====', '====')
     _float = '{:.2f}'
-    for a, b, s, d in sorted(results, key=lambda x: x[2], reverse=True):
-        cf.addrow(a, b, _float.format(s), _float.format(d))
+    for a, b, s, d, lcsl, tcsl in sorted(results, key=lambda x: x[2], reverse=True):
+        cf.addrow(a[:40], b[:40], _float.format(s), _float.format(d), str(lcsl), str(tcsl))
 
     print(str(cf))
--- tests/unit/test_util_text_distance.py
+++ tests/unit/test_util_text_distance.py
@@ -22,9 +22,11 @@
 from unittest import TestCase
 
 from util.text.distance import levenshtein
+from util.text.distance import longest_common_substring_length
 from util.text.distance import normalized_levenshtein
 from util.text.distance import string_difference
 from util.text.distance import string_similarity
+from util.text.distance import total_common_substring_length
 
 
 class TestLevenshtein(TestCase):
@@ -54,6 +56,55 @@ class TestNormalizedLevenshtein(TestCase):
         self._assert_normalized_levenshtein(0.66666666, 'foo', 'fZZ')
         self._assert_normalized_levenshtein(1.0,        'foo', 'ZZZ')
         self._assert_normalized_levenshtein(0.77777777, 'foo', 'meow meow')
+
+
+class TestLongestCommonSubstringLength(TestCase):
+    def _assert_lcs(self, expected, *given_strings):
+        actual = longest_common_substring_length(*given_strings)
+        self.assertEqual(expected, actual)
+
+    def test_completely_different_strings(self):
+        self._assert_lcs(0, 'foo', 'Z')
+        self._assert_lcs(0, 'foo', 'ZZ')
+        self._assert_lcs(0, 'foo', 'ZZZ')
+
+    def test_single_word_strings_continuous_match(self):
+        self._assert_lcs(1, 'foo', 'f')
+        self._assert_lcs(2, 'foo', 'fo')
+        self._assert_lcs(3, 'foo', 'foo')
+
+    def test_single_word_strings_multiple_possible_matches(self):
+        self._assert_lcs(4, 'fofoofooofoooo', 'fooo')
+
+    def test_multi_word_strings_continuous_match(self):
+        self._assert_lcs(1, 'foo meow baz', 'm')
+        self._assert_lcs(2, 'foo meow baz', 'me')
+        self._assert_lcs(3, 'foo meow baz', 'meo')
+        self._assert_lcs(4, 'foo meow baz', 'meow')
+        self._assert_lcs(5, 'foo meow baz', 'o meo')
+        self._assert_lcs(6, 'foo meow baz', 'oo meo')
+        self._assert_lcs(8, 'foo meow baz', 'foo meow')
+
+
+class TestTotalCommonSubstringLength(TestCase):
+    def _assert_lcs(self, expected, *given_strings):
+        actual = total_common_substring_length(*given_strings)
+        self.assertEqual(expected, actual)
+
+    def test_completely_different_strings(self):
+        self._assert_lcs(0, 'foo', 'Z')
+        self._assert_lcs(0, 'foo', 'ZZ')
+        self._assert_lcs(0, 'foo', 'ZZZ')
+
+    def test_single_word_strings_two_matching_blocks(self):
+        self._assert_lcs(2, 'foox', 'fx')
+        self._assert_lcs(3, 'bfooxaz', 'fox')
+        self._assert_lcs(3, 'bfooxaz', 'baz')
+        self._assert_lcs(4, 'bfooxaz', 'boxz')
+
+    def test_multi_word_strings_two_matching_blocks(self):
+        self._assert_lcs(2, 'foo meow baz', 'mw')
+        self._assert_lcs(4, 'foo meow baz', 'foaz')
 
 
 class TestStringDifference(TestCase):
