jonasjberg/autonameow

commit a832807714d629e3ec5174271a343def82eba853

Improve 'FiletagsExtractor' handling of dates.
Adds handling "filetags file names" containing dates without time.

Various fixes to the 'FiletagsExtractor' to highlight the fact that a
valid filetags "timestamp" can be either just a date (~"YYYY-mm-dd")
or both date and time (~"YYYY-mm-ddTHHMMSS").

Yet again the choices made when designing the system with "field meta"
it too rigid and inflexible. In this case, when a timestamp can be
either a date or a datetime, there is no clean way to declare this in
the YAML meta info file and no non-hacky ways to workaround the type
coercion system.
So, currently either none or either of 'date' and 'datetime' will be
used depending on whether the timestamp exists and if it contains a
date or both date and time __BUT__ this will raise an error which will
be debug logged most of the time, even though it is not really an
error..

autonameow/extractors/filesystem/filetags.py
@@ -40,7 +40,7 @@ FILENAME_TAG_SEPARATOR = enc.bytestring_path(' -- ')
 
 
 FiletagsParts = namedtuple('FiletagsParts',
-                           'datetime description tags extension')
+                           'timestamp description tags extension')
 
 
 class FiletagsExtractor(BaseMetadataExtractor):
@@ -69,6 +69,12 @@ class FiletagsExtractor(BaseMetadataExtractor):
 
         parts_dict = dict(parts._asdict())
         parts_dict['follows_filetags_convention'] = follows_convention
+
+        # TODO: [hack] The 'timestamp' can be either only a date or a date AND
+        #       time.. For now just copy the same value to both places and let
+        #       the coercion fail for one of them ..
+        parts_dict['date'] = parts_dict['timestamp']
+        parts_dict['datetime'] = parts_dict['timestamp']
         result = self._to_internal_format(parts_dict)
         return result
 
@@ -204,15 +210,15 @@ def follows_filetags_convention(filetags_parts):
                                 VV         V
       20160722 Descriptive name -- firsttag tagtwo.txt
       |______| |______________|    |_____________| |_|
-        date     description            tags       ext
+      timestamp  description            tags       ext
 
-    Filename parts 'date', 'description' and 'tags' must be present.
+    Filename parts 'timestamp', 'description' and 'tags' must be present.
 
     Returns:
         True if the parts probably came from a file name in the "filetags"
         format. Otherwise False.
     """
     return bool(
-        filetags_parts.datetime and filetags_parts.description
+        filetags_parts.timestamp and filetags_parts.description
         and filetags_parts.tags
     )

autonameow/extractors/filesystem/filetags_fieldmeta.yaml
@@ -1,3 +1,18 @@
+# NOTE: Either none or either of 'date' and 'datetime' will be used
+#       depending on whether the timestamp exists and if it contains
+#       a date (YYYY-mm-dd) or both date and time (YYYY-mm-ddTHHMMSS)
+date:
+    coercer: aw_date
+    generic_field: date_created
+    mapped_fields:
+    -   WeightedMapping:
+            field: Date
+            weight: 1.0
+    -   WeightedMapping:
+            field: DateTime
+            weight: 1.0
+    multivalued: false
+
 datetime:
     coercer: aw_timedate
     generic_field: date_created

tests/unit/test_extractors_filesystem_filetags.py
@@ -22,6 +22,7 @@
 from collections import namedtuple
 from datetime import datetime
 from unittest import skipIf, TestCase
+from unittest.mock import Mock
 
 import unit.utils as uu
 from extractors.filesystem.filetags import FiletagsExtractor
@@ -68,6 +69,38 @@ class TestFiletagsExtractorOutputTestFileB(CaseExtractorOutput, TestCase):
         ('description', str, 'empty'),
         ('tags', list, []),
         ('extension', str, ''),
+        ('follows_filetags_convention', bool, False)
+    ]
+
+
+def _get_mock_fileobject(filename):
+    mock_fileobject = Mock()
+    mock_fileobject.filename = filename
+    return mock_fileobject
+
+
+@skipIf(*UNMET_DEPENDENCIES)
+class TestFiletagsExtractorOutputTestFileC(CaseExtractorOutput, TestCase):
+    EXTRACTOR_CLASS = FiletagsExtractor
+    SOURCE_FILEOBJECT = _get_mock_fileobject(b'2018-06-29 kunskapsmatris sprak.xlsx')
+    EXPECTED_FIELD_TYPE_VALUE = [
+        ('date', datetime, datetime(2018, 6, 29)),
+        ('description', str, 'kunskapsmatris sprak'),
+        ('tags', list, []),
+        ('extension', str, 'xlsx'),
+        ('follows_filetags_convention', bool, False)
+    ]
+
+
+@skipIf(*UNMET_DEPENDENCIES)
+class TestFiletagsExtractorOutputTestFileD(CaseExtractorOutput, TestCase):
+    EXTRACTOR_CLASS = FiletagsExtractor
+    SOURCE_FILEOBJECT = _get_mock_fileobject(b'2007-04-23_12-comments.png')
+    EXPECTED_FIELD_TYPE_VALUE = [
+        ('date', datetime, datetime(2007, 4, 23)),
+        ('description', str, '_12-comments'),
+        ('tags', list, []),
+        ('extension', str, 'png'),
         ('follows_filetags_convention', bool, False)
     ]
 
@@ -246,46 +279,62 @@ class TestPartitionBasename(TestCase):
                     description='Advanced Data Science and Machine Learning for Cats - Paws-on Machine Learning',
                     tags=[],
                     extension='djvu')),
+
+            (b'2018-06-29 kunskapsmatris sprak.xlsx',
+             Expect(timestamp='2018-06-29',
+                    description='kunskapsmatris sprak',
+                    tags=[],
+                    extension='xlsx')),
         ]
 
     def test_partitions_basenames(self):
         for test_data, expected in self.testdata_expected:
-            actual = partition_basename(test_data)
-            self.assertEqual(expected, actual)
+            with self.subTest():
+                actual = partition_basename(test_data)
+                self.assertEqual(expected, actual)
 
 
 class TestFollowsFiletagsConvention(TestCase):
     # TODO: Clean up this mess!
     def _assert(self, expect, given):
         assert isinstance(expect, bool)
-        empty = {'datetime': '',
+        empty = {'timestamp': '',
                  'description': '',
                  'tags': [],
                  'extension': ''}
         empty.update(given)
-        parts = FiletagsParts(empty['datetime'], empty['description'], empty['tags'], empty['extension'])
-        actual = follows_filetags_convention(parts)
+
+        filetags_parts = FiletagsParts(
+            timestamp=empty['timestamp'],
+            description=empty['description'],
+            tags=empty['tags'],
+            extension=empty['extension']
+        )
+        actual = follows_filetags_convention(filetags_parts)
         self.assertEqual(expect, actual)
 
     def test_filetags_name_with_all_parts(self):
-        self._assert(True, given={'datetime': '2018-02-11',
+        self._assert(True, given={'timestamp': '2018-02-11',
                                   'description': 'foo',
                                   'tags': ['a', 'b'],
                                   'extension': 'txt'})
 
-    def test_filetags_name_with_datetime_description_tags(self):
-        self._assert(True, given={'datetime': '2018-02-11',
+    def test_filetags_name_with_timestamp_description_tags(self):
+        self._assert(True, given={'timestamp': '2018-02-11',
                                   'description': 'foo',
                                   'tags': ['a', 'b']})
 
-    def test_filetags_name_with_datetime_description_extension(self):
-        self._assert(False, given={'datetime': '2018-02-11',
+    def test_filetags_name_with_timestamp_description_extension(self):
+        self._assert(False, given={'timestamp': '2018-02-11',
                                    'description': 'foo',
                                    'extension': 'txt'})
+        self._assert(False, given={'timestamp': '2018-06-29',
+                                   'description': 'kunskapsmatris sprak',
+                                   'extension': 'xlsx'})
 
-    def test_filetags_name_with_datetime_description(self):
-        self._assert(False, given={'datetime': '2018-02-11',
+    def test_filetags_name_with_timestamp_description(self):
+        self._assert(False, given={'timestamp': '2018-02-11',
                                    'description': 'foo'})
 
-    def test_filetags_name_with_datetime(self):
-        self._assert(False, given={'datetime': '2018-02-11'})
+    def test_filetags_name_with_timestamp(self):
+        self._assert(False, given={'timestamp': '2018-02-11'})

commit 76780389c313ab4b50db0cd710e330ea09ac06c6

Remove filetags 'timestamp' after copying.
Fixes bug introduced in commit a832807714d629e3ec5174271a343def82e

Removes the 'timestamp' from the "raw metadata" dict after copying
its value to both 'date' and 'datetime'.
Fixes logging debug warnings:  Field not in "metainfo"; "timestamp"

autonameow/extractors/filesystem/filetags.py
@@ -111,8 +111,11 @@ class FiletagsExtractor(BaseMetadataExtractor):
         # TODO: [hack] The 'timestamp' can be either only a date or a date AND
         #       time.. For now just copy the same value to both places and let
         #       the coercion fail for one of them ..
-        raw_metadata['date'] = raw_metadata['timestamp']
-        raw_metadata['datetime'] = raw_metadata['timestamp']
+        if 'timestamp' in raw_metadata:
+            raw_metadata['date'] = raw_metadata['timestamp']
+            raw_metadata['datetime'] = raw_metadata['timestamp']
+            del raw_metadata['timestamp']
+
         return raw_metadata
 
     def _to_internal_format(self, raw_metadata):