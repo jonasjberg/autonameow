aa380c52e6ed6a8216b7f07c712f0d3e0cb2a057
Jonas Sjöberg <jomeganas@gmail.com>
2018-10-24 15:02:36 +0200

    Use bold text for integration test results.

--- tests/integration/utils.sh
+++ tests/integration/utils.sh
@@ -22,6 +22,7 @@ set -o noclobber -o nounset -o pipefail
 
 C_RED="$(tput setaf 1)"
 C_GREEN="$(tput setaf 2)"
+C_BOLD="$(tput bold)"
 C_RESET="$(tput sgr0)"
 
 
@@ -96,15 +97,15 @@ log_test_suite_results_summary()
     _highlight_red=''
     if [ "$suite_tests_failed" -eq "0" ]
     then
-        logmsg "${C_GREEN}[ ALL TESTS PASSED ]${C_RESET}"
+        logmsg "${C_BOLD}${C_GREEN}[ ALL TESTS PASSED ]${C_RESET}"
     else
-        logmsg "${C_RED}[ SOME TESTS FAILED ]${C_RESET}"
+        logmsg "${C_BOLD}${C_RED}[ SOME TESTS FAILED ]${C_RESET}"
         _highlight_red="${C_RED}"
     fi
 
-    logmsg "$(printf "Test Suite Summary:  %d total, %d passed, ${_highlight_red}%d failed${C_RESET}" \
+    logmsg "$(printf "${C_BOLD}Test Suite Summary:  %d total, %d passed, ${_highlight_red}%d failed${C_RESET}" \
               "$suite_tests_count" "$suite_tests_passed" "$suite_tests_failed")"
-    logmsg "Completed the ${_name} test suite tests in ${_execution_time} ms"
+    logmsg "${C_BOLD}Completed the ${_name} test suite tests in ${_execution_time} ms${C_RESET}"
     logmsg "======================================================================"
 }
 
@@ -123,9 +124,9 @@ log_total_results_summary()
     _highlight_red=''
     if [ "$_tests_failed" -eq "0" ]
     then
-        logmsg "${C_GREEN}[ ALL TEST SUITE(S) TESTS PASSED ]${C_RESET}"
+        logmsg "${C_BOLD}${C_GREEN}[ ALL TEST SUITE(S) TESTS PASSED ]${C_RESET}"
     else
-        logmsg "${C_RED}[ SOME TEST SUITE(S) TESTS FAILED ]${C_RESET}"
+        logmsg "${C_BOLD}${C_RED}[ SOME TEST SUITE(S) TESTS FAILED ]${C_RESET}"
         _highlight_red="${C_RED}"
     fi
 
@@ -137,9 +138,9 @@ log_total_results_summary()
                $((_seconds % 3600 / 60))      \
                $((_seconds % 60)))"
 
-    logmsg "$(printf "Total Test Summary:  %d total, %d passed, ${_highlight_red}%d failed${C_RESET}" \
+    logmsg "$(printf "${C_BOLD}Total Test Summary:  %d total, %d passed, ${_highlight_red}%d failed${C_RESET}" \
               "$_tests_count" "$_tests_passed" "$_tests_failed")"
-    logmsg "Completed all tests in ${_duration}  (${_execution_time} ms)"
+    logmsg "${C_BOLD}Completed all tests in ${_duration}  (${_execution_time} ms)${C_RESET}"
     logmsg "======================================================================"
 }
 
