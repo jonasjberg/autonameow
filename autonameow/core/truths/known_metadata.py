6e7defbdf9f013436e063e77877314406a578f23
Jonas Sjöberg <jomeganas@gmail.com>
2019-01-03 01:11:41 +0100

    Add ability to get known incorrect field values.
    Adds new function 'incorrect_values()' with unit tests.
    
    Returns values known to be incorrect for the given field name.
    
    Accepts a field value that corresponds to the basenames of files
    stored in the 'core/truths/data' directory and returns a set of
    all values known to be incorrect for the given field name.

--- autonameow/core/truths/known_metadata.py
+++ autonameow/core/truths/known_metadata.py
@@ -22,3 +22,32 @@ from core.truths import known_data_loader
 
 def canonical_values(fieldname):
     return known_data_loader.lookup_values(fieldname) or set()
+
+
+# TODO: Get valid fieldnames dynamically from the known data loader.
+VALID_FIELDNAMES = frozenset(['creatortool', 'language', 'publisher'])
+
+
+def incorrect_values(fieldname):
+    """
+    Returns values known to be incorrect for the given fieldname.
+
+    Args:
+        fieldname (str): Field value corresponding to the basenames of files
+                         stored in the 'data' directory, as a Unicode string.
+
+    Returns:
+        A set of all values known to be incorrect for the given field name.
+    """
+    if fieldname not in VALID_FIELDNAMES:
+        return set()
+
+    other_fieldnames = [n for n in VALID_FIELDNAMES if n != fieldname]
+
+    values = set()
+    for other_fieldname in other_fieldnames:
+        values.update(known_data_loader.lookup_values(other_fieldname))
+        lookup = known_data_loader.literal_lookup_dict(other_fieldname)
+        values.update(*lookup.values())
+
+    return values
--- tests/unit/test_core_truths_known_metadata.py
+++ tests/unit/test_core_truths_known_metadata.py
@@ -17,9 +17,10 @@
 #   You should have received a copy of the GNU General Public License
 #   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
 
-from unittest import TestCase
+from unittest import expectedFailure, TestCase
 
 from core.truths.known_metadata import canonical_values
+from core.truths.known_metadata import incorrect_values
 
 
 class TestCanonicalValues(TestCase):
@@ -31,3 +32,34 @@ class TestCanonicalValues(TestCase):
         actual = canonical_values('publisher')
         self.assertIsInstance(actual, set)
         self.assertGreater(len(actual), 0)
+
+    @expectedFailure
+    def test_does_not_return_unknowns(self):
+        actual = canonical_values('publisher')
+        self.assertNotIn('UNKNOWN', actual)
+
+
+class TestIncorrectValues(TestCase):
+    def test_returns_empty_set_given_bad_fieldname(self):
+        actual = incorrect_values('foobarbaz')
+        self.assertIsInstance(actual, set)
+
+    def test_returns_non_empty_set_given_valid_fieldname(self):
+        actual = incorrect_values('publisher')
+        self.assertIsInstance(actual, set)
+        self.assertGreater(len(actual), 0)
+
+    def test_returns_known_bad_creatortool_value(self):
+        actual = incorrect_values('creatortool')
+        self.assertIn('ProjectGutenberg', actual)
+        self.assertIn('SWEDISH', actual)
+
+    def test_returns_known_bad_language_value(self):
+        actual = incorrect_values('language')
+        self.assertIn('Calibre', actual)
+        self.assertIn('Project Gutenberg', actual)
+
+    def test_returns_known_bad_publisher_value(self):
+        actual = incorrect_values('publisher')
+        self.assertIn('Calibre', actual)
+        self.assertIn('SWEDISH', actual)
